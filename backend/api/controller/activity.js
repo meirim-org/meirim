0c630a9b88c739af2bef3c0842bd5d37c86b59fc
Jonathan Dortheimer <jonathan@dortheimer.com>
2017-08-29 13:52:43 +0300

    Added postman collection

--- backend/api/controller/activity.js
+++ backend/api/controller/activity.js
@@ -3,8 +3,22 @@ const Router = require('express').Router();
 const Controller = require("../controller/controller");
 const Activity = require("../model/activity");
 const _ = require('lodash');
-class ActivityController extends Controller {}
+class ActivityController extends Controller {
+	create(req, res, next) {
+
+		return this.model.canCreate(req.session).then(()=>{
+			return Bookshelf.transaction(t=> {
+					return this.mode.forge(req.body).save(null, {transacting: t}).tap(savedModel=> {
+						savedModel.addPerson(req.session);
+					});
+				});
+		});
+	}
+}
+
+
 var controller = new ActivityController(Activity);
+
 Router.get('/', (req, res, next) => {
 	controller.wrap(_.bind(controller.browse, controller))(req, res, next);
 });
