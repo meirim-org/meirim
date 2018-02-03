6f30995f709d4def8239e06c6c3b73944de02e1e
naortor <naortor@gmail.com>
2018-02-03 19:09:19 +0200

    facebook link in footer

--- client/src/components/home_page/components/footer/footer.jsx
+++ client/src/components/home_page/components/footer/footer.jsx
@@ -5,10 +5,15 @@ require('./footer.scss');
 const Footer = () => {
   return (
     <div className={"footer"}>
-      <p className={"conatct-us"}>מוזמנים לכתוב לנו בכל עניין
-        <br/>
-        <a href={"mailto:info@mairim.com"}>info@meirim.com</a>
-      </p>
+      <div className={"conatct-us"}>
+        <div className='mail'>
+          <p>מוזמנים לכתוב לנו בכל עניין</p>
+          <a href={"mailto:info@mairim.com"}>info@meirim.com</a>
+        </div>
+        <a href='https://www.facebook.com/meirim.city/' target='_blank'>
+          <img src={require('../../../../images/logo-fb.png')} />
+        </a>
+      </div>
       <p className={"copyrights"}>
         © #{(new Date()).getFullYear()}   כל הזכויות שמורות למעירים
       </p>
