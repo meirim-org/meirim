87df6c5e26a21e98999433b1d82f2c17442155a8
naortor <naortor@gmail.com>
2018-02-05 21:32:09 +0200

    login and redirect to alerts, fixed signup

--- client/build-scripts/dev.js
+++ client/build-scripts/dev.js
@@ -11,7 +11,12 @@ const options = {
   stats: {
     colors: true
   },
-  historyApiFallback: true
+  historyApiFallback: {
+    rewrites: [{
+      from: '/alerts',
+      to: '/alert.html'
+    }]
+  }
 };
 
 const server = new WebpackDevServer(webpack(webpackConfig), options);
