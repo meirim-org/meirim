2986993d347886ba0dd0a252d599235d9735fc13
Jonathan Dortheimer <jonathan.dortheimer@sergata.com>
2018-05-07 20:11:21 +0300

    Fixed pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,11 @@
 module.exports = {
-  apps : [{
-    name        : "api",
-    script      : "bin/api",
-    watch       : true,
+  apps: [{
+    name: 'api',
+    script: 'bin/api',
+    watch: true,
     env: {
-      "NODE_ENV": "production",
-      "PORT":3000
+      'NODE_ENV': 'production',
+      'PORT': 3000
     }
-  },
-  // {
-  //   name        : "client",
-  //   script      : "node_modules/react-server-cli/target/cli.js",
-  //   watch       : true,
-  //   cwd         : "client/",
-  //   args   : 'start',
-  //   ignore_watch: '__clientTemp',
-  //   env: {
-  //     "NODE_ENV": "production",
-  //     REACT_SERVER_CONFIGS: "_configs/production/",
-  //     "PORT":3001
-  //   }
-  // }
-]
+  }]
 }
