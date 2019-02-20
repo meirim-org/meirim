726e42a716b7841e6b3b3de4b439427bef668de1
Gal Gendler <ggendler@gmail.com>
2019-02-20 20:38:06 +0200

    add migration

---
+++ migrations/20190220203303_add_jurisdiction.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.table('plan', (table) => {
+      table.text('jurisdiction');
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.table('plan', (table) => {
+      table.dropColumns('jurisdiction');
+    });
+  };
+  
