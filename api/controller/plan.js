5cc11e4218412eb2e9220d834d4300174dddb93e
Jonathan Dortheimer <jonathan@dortheimer.com>
2019-02-27 20:22:20 +0200

    Added plan query

--- api/controller/plan.js
+++ api/controller/plan.js
@@ -2,6 +2,33 @@ const Controller = require('../controller/controller');
 const Plan = require('../model/plan');
 class PlanController extends Controller {
 
+  browse(req) {
+    
+    const columns = [
+      'PLAN_COUNTY_NAME',
+      'PL_NUMBER',
+      'PL_NAME',
+      'PLAN_CHARACTOR_NAME',
+      'goals_from_mavat',
+      'main_details_from_mavat',
+    ];
+    
+    const {query} = req;
+    const where = {};
+
+    if (query.status) {
+      where.status = query.status.split(',');
+    }
+
+    if (query.PLAN_COUNTY_NAME) {
+      where.PLAN_COUNTY_NAME = query.PLAN_COUNTY_NAME.split(',');
+    }  
+
+    return super.browse(req, {
+      columns,
+      where
+    })
+  }
 }
 
 module.exports = new PlanController(Plan);
