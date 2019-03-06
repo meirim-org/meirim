540d7a15f3ec6b66f9d602caeaf75425bbcae3db
Gal Gendler <ggendler@gmail.com>
2019-03-06 19:52:46 +0200

    fixed the migration

--- migrations/20190306194602_add_plan_user.js
+++ migrations/20190306194602_add_plan_user.js
@@ -1,12 +1,10 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.table('plan_person', (table) => {
+    return knex.schema.createTable('plan_person', (table) => {
         table.integer('person_id');
         table.integer('plan_id');
     });
   };
   
   exports.down = function(knex, Promise) {
-    return knex.schema.table('plan_person', (table) => {
-      table.dropColumns('plan_id', 'person_id');
-    });
+    return knex.schema.dropTab('plan_person');
   };
