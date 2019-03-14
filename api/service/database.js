399b3dd8eda678d9ab190820c59a919e09f5e03c
Gal Gendler <ggendler@gmail.com>
2019-03-14 18:57:44 +0200

    fixed config location

--- api/service/database.js
+++ api/service/database.js
@@ -1,10 +1,10 @@
+const Config = require('../lib/config');
 const Knex = require('knex')({
   client: Config.get('database.client'),
   connection: Config.get('database.connection'),
   debug: Config.get('debug.database'),
 });
 const Bookshelf = require('bookshelf')(Knex);
-const Config = require('../lib/config');
 
 Bookshelf.plugin(['visibility', 'pagination']);
 Bookshelf.plugin(require('../lib/bookshelf-mysql-gis'));
