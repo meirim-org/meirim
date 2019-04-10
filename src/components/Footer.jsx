82ab7c00c0ee679096385720dd8855d6f6fd8575
florpor <florist.powers@gmail.com>
2019-04-10 21:38:50 +0300

    fix footer spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,9 @@ class Footer extends Component {
   render() {
       return <footer className="text-center">
         
-      <a href="mailto:info@meirim.org">יצירת קשר</a> | <a href="https://www.facebook.com/meirim.city/">הפייסבוק שלנו</a> |
-      <Link to="/about/">{t.about}</Link> | 
+      <a href="mailto:info@meirim.org">יצירת קשר</a> |&nbsp; 
+      <a href="https://www.facebook.com/meirim.city/">הפייסבוק שלנו</a> |&nbsp; 
+      <Link to="/about/">{t.about}</Link> |&nbsp; 
       <a href="https://github.com/meirim-org/meirim/">הצטרפו לצוות</a><br />
       <Link to="/terms/">תנאי שימוש</Link> |  כל הזכויות שמורות
     </footer>
