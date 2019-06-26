242c5fff7d49c24fe0c6ceb68e626b745d204cc3
Jonathan Dortheimer <jonathan@dortheimer.com>
2019-06-26 21:02:38 +0300

    Fixed home page text

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -1,61 +1,62 @@
-import React, { Component } from 'react';
-import Navigation from '../components/Navigation';
-import Footer from '../components/Footer';
-import RegisterForm from '../components/RegisterForm';
+import React, { Component, Fragment } from "react";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import RegisterForm from "../components/RegisterForm";
 
-import t from '../locale/he_IL';
-import traktor from '../assets/traktor_op.png';
-import logoSmall from '../assets/logo_small.png';
-import './Home.css';
+import t from "../locale/he_IL";
+import traktor from "../assets/traktor_op.png";
+import logoSmall from "../assets/logo_small.png";
+import "../assets/bootstrap.css";
+import "./Home.css";
 
-class Home extends Component {
-  render() {
-    const { me } = this.props;
+export default function Home(props) {
+    const { me } = props;
     return (
-      <React.Fragment>
-        <Navigation me={me} />
-
-        <div className="container heroContainer">
-          <section className="hero">
-            <img className="img-fluid background" src={traktor} alt="" />
-            <div className="content">
-              <img className="logo" src={logoSmall} alt={t.name} />
-              <div className="d-lg-none">
-                <h4>{t.meirimTitle}</h4>
-                <p>רוצים השכמה?</p>
-                <a href="#register" className="join">
-                  הצטרפו למעירים
-                </a>
-              </div>
+        <Fragment>
+            <Navigation me={me} />
+            <div
+                className="d-none d-md-block"
+                style={{
+                    width: "50%",
+                    position: "absolute",
+                    width: "50%",
+                    height: "100%",
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                    backgroundImage: `url(${traktor})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "50% 50%"
+                }}
+            />
+            <div className="container heroContainer">
+                <section className="hero">
+                    <div className="content">
+                        <img className="logo" src={logoSmall} alt={t.name} />
+                        <div className="d-lg-none">
+                            <h4>{t.meirimTitle}</h4>
+                            <p>{t.whatToRegister}</p>
+                            <a href="#register" className="join">
+                                {t.callToAction}
+                            </a>
+                        </div>
+                    </div>
+                </section>
+                <div className="row garden">
+                    <div className="col-lg-4">
+                        <h2>{t.whyRegister}</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <p>{t.howItWorks}</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <RegisterForm />
+                    </div>
+                </div>
             </div>
-          </section>
-          <div className="row garden">
-            <div className="col-lg-4">
-              <h2>
-                רוצים לדעת אם הגינה הציבורית שלכם עומדת{' '}
-                <strong>להפוך למגדל?</strong>
-              </h2>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-4">
-              <p>
-                מערכת ההתראות שלנו מאגדת במקום אחד את כל התכניות בסמכות מקומית
-                ומחוזית ותשלח לכם התראה למייל בזמן אמת, בכל פעם שתופקד תכנית
-                חדשה בסמכות מקומית או כשתקלט במערכת תכנית חדשה בסמכות מחוזית
-                באזור מגוריכם או בכל אזור אחר שתבחרו! כך תוכלו לעקוב בקלות אחרי
-                השינויים בסביבת המגורים שלכם.
-              </p>
-            </div>
-            <div className="col-lg-4">
-              <RegisterForm />
-            </div>
-          </div>
-        </div>
-        <Footer />
-      </React.Fragment>
+            <Footer />
+        </Fragment>
     );
-  }
 }
-
-export default Home;
