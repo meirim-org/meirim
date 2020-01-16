2b22496959797ddf48f2ccff12972b6250ceac14
Gal Gendler <ggendler@gmail.com>
2020-01-16 21:18:05 +0000

    fixed migrations

--- migrations/20171030203050_init.js
+++ migrations/20171030203050_init.js
@@ -1,3 +1,4 @@
+const Promise = require('bluebird')
 const tables = [
   `CREATE TABLE activity (
     id int(10) UNSIGNED NOT NULL,
@@ -55,13 +56,6 @@ const tables = [
     PLAN_CHARACTOR_NAME varchar(256) COLLATE utf8_bin NOT NULL,
     data text COLLATE utf8_bin NOT NULL,
     geom geometry NOT NULL
-    jurisdiction varchar(256) COLLATE utf8_bin,
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;`,
-
-  `CREATE TABLE plan_person (
-    plan_id int(11) NOT NULL,
-    person_id int(11) NOT NULL,
-    follow tinyint(4),
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
 
   `CREATE TABLE post (
@@ -91,7 +85,7 @@ const statements = [
   'ALTER TABLE person MODIFY id int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;',
 ];
 
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return Promise.all(
     tables.map(table => {
       try {
