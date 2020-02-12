3be52a89ac0ef28f3382dedb155ca6171ccd7c83
Fusion <fusiongen2020@outlook.com>
2020-02-12 08:07:41 +0200

    Fix.

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,11 +1,11 @@
-const Router = require("express").Router();
-const SignUp = require("./controller/sign");
-const Password = require("./controller/password");
-const Alert = require("./controller/alert");
-const Plan = require("./controller/plan");
-const Comment = require("./controller/comment");
-const Rate = require("./controller/rate");
-const {publicCors} = require("../lib/cors");
+const Router = require("express").Router()
+const SignUp = require("./controller/sign")
+const Password = require("./controller/password")
+const Alert = require("./controller/alert")
+const Plan = require("./controller/plan")
+const Comment = require("./controller/comment")
+const Rate = require("./controller/rate")
+const {publicCors} = require("../lib/cors")
 
 const Subscription = require("./controller/subscription");
 // const Tag = require('./controller/tag');
@@ -14,50 +14,50 @@ const Subscription = require("./controller/subscription");
 const { wrap, publicWrapper } = require("./controller/controller");
 
 // Sign up
-Router.post("/sign/up", wrap(SignUp.signup, SignUp));
-Router.post("/sign/activate", wrap(SignUp.activate, SignUp));
-Router.post("/sign/in", wrap(SignUp.signin, SignUp));
-Router.post("/sign/out", wrap(SignUp.signout, SignUp));
+Router.post('/sign/up', wrap(SignUp.signup, SignUp))
+Router.post('/sign/activate', wrap(SignUp.activate, SignUp))
+Router.post('/sign/in', wrap(SignUp.signin, SignUp))
+Router.post('/sign/out', wrap(SignUp.signout, SignUp))
 
 // Plan
-Router.get("/plan/", wrap(Plan.browse, Plan));
-Router.get("/plan/:id", wrap(Plan.read, Plan));
+Router.get('/plan/', wrap(Plan.browse, Plan))
+Router.get('/plan/:id', wrap(Plan.read, Plan))
 
-Router.get("/plan_county", wrap(Plan.county, Plan));
-Router.get("/plan_status", wrap(Plan.statuses, Plan));
+Router.get('/plan_county', wrap(Plan.county, Plan))
+Router.get('/plan_status', wrap(Plan.statuses, Plan))
 
-Router.post("/plan/:id/subscribe", wrap(Subscription.subscribe, Subscription));
+Router.post('/plan/:id/subscribe', wrap(Subscription.subscribe, Subscription));
 Router.delete(
     "/plan/:id/subscribe",
     wrap(Subscription.unsubscribe, Subscription)
 );
 
 // Comment
-Router.get("/comment/:plan_id", wrap(Comment.byPlan, Comment));
-Router.post("/comment/:plan_id", wrap(Comment.create, Comment));
+Router.get('/comment/:plan_id', wrap(Comment.byPlan, Comment))
+Router.post('/comment/:plan_id', wrap(Comment.create, Comment))
 
 // Rate
-Router.get("/rate/:plan_id", wrap(Rate.byPlan, Rate));
-Router.post("/rate/", wrap(Rate.create, Rate));
+Router.get('/rate/:plan_id', wrap(Rate.byPlan, Rate));
+Router.post('/rate/', wrap(Rate.create, Rate));
 
 // Password
-Router.post("/password/sendResetToken", wrap(Password.sendResetToken));
-Router.post("/password/resetWithToken", wrap(Password.resetWithToken));
+Router.post('/password/sendResetToken', wrap(Password.sendResetToken))
+Router.post('/password/resetWithToken', wrap(Password.resetWithToken))
 
 // Alert
-Router.get("/alert/", wrap(Alert.browse, Alert));
-Router.get("/alert/:id", wrap(Alert.read, Alert));
-Router.post("/alert/", wrap(Alert.create, Alert));
-Router.delete("/alert/:id", wrap(Alert.delete, Alert));
-Router.delete("/alert/_token/:token", wrap(Alert.unsubscribe, Alert));
+Router.get('/alert/', wrap(Alert.browse, Alert))
+Router.get('/alert/:id', wrap(Alert.read, Alert))
+Router.post('/alert/', wrap(Alert.create, Alert))
+Router.delete('/alert/:id', wrap(Alert.delete, Alert))
+Router.delete('/alert/_token/:token', wrap(Alert.unsubscribe, Alert))
 
 // me
-Router.get("/me/", wrap(Alert.browse, Alert));
+Router.get('/me/', wrap(Alert.browse, Alert))
 
 // Public API
-Router.use("/public", publicCors());
+Router.use('/public', publicCors())
 
-Router.get("/public/plan", publicWrapper(Plan.publicBrowse, Plan));
+Router.get('/public/plan', publicWrapper(Plan.publicBrowse, Plan))
 
 // Router.get('/cron/send_planning_alerts', wrap(sendPlanningAlerts));
 
@@ -73,4 +73,4 @@ Router.get(
     wrap(() => true)
 );
 
-module.exports = Router;
+module.exports = Router
