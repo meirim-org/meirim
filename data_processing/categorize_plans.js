48134d6e1b9c13d3eb0c09c129ccf782bceaa242
TomerMe2 <tomer.laor1@gmail.com>
2020-07-30 02:45:42 +0300

    added the categorization algorithm and tests

---
+++ data_processing/categorize_plans.js
@@ -0,0 +1,162 @@
+const readXlsxFile = require('read-excel-file/node');
+const path = require('path');
+
+
+
+//TODO: ADD REF TO https://yeda.cs.technion.ac.il/resources_lexicons_stopwords.html in the readme
+//TODO: ADD MIGRATIONS THAT WILL CREATE AND POPULATE plan_area_changes table in DB
+
+
+
+//returns a set of hebrew words
+//we need this function to search for words that has prefix or suffix. for example: למתחם is מתחם, with the list of words
+//we can know that למתחם contains a prefix or a suffix.
+async function readStopWords() {
+    // `rows` is an array of rows
+    // each row being an array of cells
+    const pth = path.join(__dirname, 'resources', 'stopwords.xlsx');
+    const rows = await readXlsxFile(pth);
+    rows.shift();     //remove the headers
+    const words = rows.map(row => row[2]);
+    const wordsSet = new Set(words);
+    wordsSet.delete('מדרך');   // it's a weird word and it's in there....
+    wordsSet.add('מתחם');           //important words in the domain and it's not in the stopwords
+    return wordsSet;
+}
+
+function includesOrList(sentence, words) {
+    return words.reduce((acc, currWord) => acc || sentence.includes(currWord), false);
+}
+
+function notIncludes(sentence, word) {
+    return !sentence.includes(word);
+}
+
+function includes(sentence, word) {
+    return sentence.includes(word);
+}
+
+function tagDetail(tag, detail) {
+    return {tag: tag, detail: detail};
+}
+
+function parseAreaDesignationChange(detail, stopWordsSet) {
+    let currWord = '';
+    let gatherWord = '';
+    let fromIndex = -1;
+    let toIndex = -1;
+    let numberOfTimesChangedToIndex = 0;
+
+    for(let i = 0; i < detail.length; i++) {
+        const currChar = detail.charAt(i);
+
+        if (currWord === ' ' || i === detail.length - 1) {
+            //end of the word or the end of the sentence
+            currWord = gatherWord;
+            gatherWord = '';
+            //from now we will check if this word contains the area changes (from... to...)
+
+            //clean the word
+            const wordToSearch = currWord.replace(',', '')
+                                         .replace('(', '')
+                                         .replace(')', '');
+
+            if (fromIndex === -1 && currWord.startsWith('מ') && !stopWordsSet.has(wordToSearch)) {
+                //didn't find from yet, starts with 'מ' and not in the stopWords (eliminates words like 'מנורה')
+                //contains the from... area
+                //fromIndex will be the index of the first letter of this word in the sentence
+                fromIndex = i - currWord.length + 1;
+            }
+            else if (fromIndex !== -1 && currWord.startsWith('ל') && !stopWordsSet.has(wordToSearch)) {
+                //contains the to... area
+                //already found from, starts with 'ל' and not in the stopWords (eliminates words like 'לימון')
+                // it's < 2 for situation like: דרך להולכי רגל ב
+                if (numberOfTimesChangedToIndex < 2) {
+                    toIndex = i - currWord.length + 1;
+                }
+                numberOfTimesChangedToIndex++;
+            }
+        }
+        else {
+            //it's not the end of the word, gather that char
+            gatherWord = gatherWord.concat(currChar);
+        }
+    }
+    if (fromIndex !== -1 && toIndex !== -1 && fromIndex < toIndex) {
+        const fromArea = detail.slice(fromIndex, toIndex - 2);
+        const toArea = detail.slice(toIndex).replace('.', '');
+        return {tag: 'AreaDesignationChange', fromArea: fromArea, toArea: toArea, detail: detail};
+    }
+    else {
+        return {tag: 'AreaDesignationChange', fromArea: '', toArea: '', detail: detail};
+    }
+}
+
+function parseDetail(detail, stopWordsSet) {
+    if(includesOrList(detail, ['הפקעה', 'הפקעות'])) {
+        return tagDetail('Expropriation', detail);
+    }
+    if(includes(detail, 'הנאה')) {
+        return tagDetail('Movement', detail);
+    }
+    if(includesOrList(detail, ['שינוי יעוד', 'שינוי ייעוד', 'יעוד קרקע']) ||
+        (includes(detail, 'שינוי') &&
+            includesOrList(detail, ['איזור', 'אזור']) &&
+            notIncludes(detail, 'גודל') &&
+            notIncludes(detail, 'גובה'))) {
+        return parseAreaDesignationChange(detail, stopWordsSet);
+    }
+    if(includes(detail, 'שימושים מותרים') ||
+            (includesOrList(detail, ['קביעת', 'הגדרת']) && includes(detail, 'שימושים'))) {
+        return tagDetail('LandUse', detail);
+    }
+    if(includes(detail, 'עצים')) {
+        return tagDetail('Trees', detail);
+    }
+    if(includes(detail, 'שימור')) {
+        return tagDetail('Preservation', detail);
+    }
+    if(includesOrList(detail, ['הריסה', 'הריסת', 'הריסות'])) {
+        return tagDetail('Destruction', detail);
+    }
+    if(includes(detail, 'היתר')) {
+        return tagDetail('Permit', detail);
+    }
+    if(includes(detail, 'עיצוב אדריכלי')) {
+        return tagDetail('Architectural', detail);
+    }
+    if(includes(detail, 'שלבי')) {
+        return tagDetail('Step', detail);
+    }
+    if(includesOrList(detail, ['איחוד', 'חלוקה'])) {
+        return tagDetail('UnionAndDivision', detail);
+    }
+    if(includes(detail, 'קווי בניין') ||
+        (includes(detail,'שינוי') && includesOrList(detail, ['בניין', 'בנין'])) ||
+        (includes(detail,'קו') && includesOrList(detail, ['בנין', 'בניין']))) {
+        return tagDetail('Building', detail);
+    }
+    if(includes(detail, 'תכסית')) {
+        return tagDetail('Building', detail);
+    }
+    if(includesOrList(detail, ['זכויות בנייה', 'זכויות בניה'])) {
+        return tagDetail('Building', detail);
+    }
+    if(includesOrList(detail, ['הגדלת', 'הקטנת', 'תוספת'])) {
+        return tagDetail('Building', detail);
+    }
+    if(includes(detail, 'שימושים')) {
+        return tagDetail('Building', detail);
+    }
+    if(includesOrList(detail, ['בינוי', 'בנייה', 'בניה'])) {
+        return tagDetail('Building', detail);
+    }
+    if(includes(detail, 'קביעת')) {   // default for קביעת
+        return tagDetail('Building', detail);
+    }
+}
+
+module.exports = {
+    parseDetail,
+    readStopWords
+};
---
+++ test/data_processing/categorize_palns.test.js
@@ -0,0 +1,169 @@
+const Classifier = require('../../data_processing/categorize_plans');
+const assert = require('assert');
+
+//all the tests are from actual tabas
+
+describe('classification test', function() {
+    this.timeout(90000);   //reading the stop words is taking a long time
+    let stopWordsSet;
+
+    before(async () => {
+        stopWordsSet = await Classifier.readStopWords();
+    });
+
+    it('should be tagged as Expropriation', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות להפקעה', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Expropriation');
+    });
+
+    it('should be tagged as Expropriation', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות הפקעה לשטח ציבורי פתוח.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Expropriation');
+    });
+
+    it('should be tagged as Expropriation', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות בגין הפקעה לצרכי ציבור .', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Expropriation');
+    });
+
+    it('should be tagged as Movement', () => {
+        const obj = Classifier.parseDetail('קביעת זיקת הנאה למעבר ברכב בתחום השצ"פ.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Movement');
+    });
+
+    it('should be tagged as Movement', () => {
+        const obj = Classifier.parseDetail('קביעת זיקות הנאה למעבר רגלי במגרש מגורים ד\'.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Movement');
+    });
+
+    it('should be tagged as Movement', () => {
+        const obj = Classifier.parseDetail('קביעת שטחים והוראות לזיקת הנאה לציבור.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Movement');
+    });
+
+    it('should be tagged as AreaDesignationChange', () => {
+        const obj = Classifier.parseDetail('שינוי יעוד משצ"פ לדרך מוצעת.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'AreaDesignationChange');
+    });
+
+    it('should be tagged as AreaDesignationChange', () => {
+        const obj = Classifier.parseDetail('שינוי ייעוד הקרקע ממגורים 5 מיוחד למגורים ב\'.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'AreaDesignationChange');
+    });
+
+    it('should be tagged as AreaDesignationChange', () => {
+        const obj = Classifier.parseDetail('שינוי יעוד מאזור מגורים ד\'1 למגורים ד\'', stopWordsSet);
+        assert.strictEqual(obj.tag, 'AreaDesignationChange');
+    });
+
+    it('should be tagged as LandUse', () => {
+        const obj = Classifier.parseDetail('הגדרת שימושים מותרים לשטח השצ"פ.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'LandUse');
+    });
+
+    it('should be tagged as LandUse', () => {
+        const obj = Classifier.parseDetail('קביעת השימושים המותרים.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'LandUse');
+    });
+
+    it('should be tagged as Trees', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות בגין נטיעת עצים בוגרים', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Trees');
+    });
+
+    it('should be tagged as Trees', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות לשימור, נטיעה, העתקה וכריתת  עצים בוגרים.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Trees');
+    });
+
+    it('should be tagged as Trees', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות בגין עצים לשימור.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Trees');
+    });
+
+    it('should be tagged as Preservation', () => {
+        const obj = Classifier.parseDetail('קביעת מבנה לשימור ומתן הנחיות לשימור, בנייה ופיתוח בסביבתו.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Preservation');
+    });
+
+    it('should be tagged as Preservation', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות לשימור מבנים ואתרים היסטוריים.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Preservation');
+    });
+
+    it('should be tagged as Destruction', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות בגין מבנים מדרגות וגדרות להריסה.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Destruction');
+    });
+
+    it('should be tagged as Destruction', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות בגין סככה,מבנה להריסה.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Destruction');
+    });
+
+    it('should be tagged as Permit', () => {
+        const obj = Classifier.parseDetail('קביעת תנאים למתן היתר בניה.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Permit');
+    });
+
+    it('should be tagged as Permit', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות בנוי ופיתוח וקביעת תנאים למתן היתר בניה.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Permit');
+    });
+
+    it('should be tagged as Architectural', () => {
+        const obj = Classifier.parseDetail('שינוי של הוראות לפי תכנית בדבר בינוי או עיצוב אדריכליים לפי סעיף 62א (א) (5) לחוק.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Architectural');
+    });
+
+    it('should be tagged as Architectural', () => {
+        const obj = Classifier.parseDetail('שינוי הוראות בינוי /עיצוב אדריכלי בהתאם לסעיף 62א(א)5.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Architectural');
+    });
+
+    it('should be tagged as Step', () => {
+        const obj = Classifier.parseDetail('קביעת שלבי ביצוע למימוש התכנית.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Step');
+    });
+
+    it('should be tagged as Step', () => {
+        const obj = Classifier.parseDetail('קביעת שלבי ביצוע להקמת הבניה , כאמור.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Step');
+    });
+
+    it('should be tagged as UnionAndDivision', () => {
+        const obj = Classifier.parseDetail('קביעת הוראות להכנת תכנית איחוד וחלוקה בתכנית עתידית.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'UnionAndDivision');
+    });
+
+    it('should be tagged as UnionAndDivision', () => {
+        const obj = Classifier.parseDetail('איחוד וחלוקה של מגרשים לפי סעיף 62 א (א) 1 לחוק התכנון והבניה.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'UnionAndDivision');
+    });
+
+    it('should be tagged as UnionAndDivision', () => {
+        const obj = Classifier.parseDetail('חלוקה בהסכמה', stopWordsSet);
+        assert.strictEqual(obj.tag, 'UnionAndDivision');
+    });
+
+    it('should be tagged as Building', () => {
+        const obj = Classifier.parseDetail('שינוי בקווי בניין :', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Building');
+    });
+
+    it('should be tagged as Building', () => {
+        const obj = Classifier.parseDetail('שינוי בקו בניין קדמי מ- 3.0 לפי המסומן בתשריט.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Building');
+    });
+
+    it('should be tagged as Building', () => {
+        const obj = Classifier.parseDetail('הגדלת תכסית קרקע מ-36% ל- 45%', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Building');
+    });
+
+    it('should be tagged as Building', () => {
+        const obj = Classifier.parseDetail('שינוי הוראות בדבר בינוי לפי סעיף 62 א (א)5.', stopWordsSet);
+        assert.strictEqual(obj.tag, 'Building');
+    });
+
+});
