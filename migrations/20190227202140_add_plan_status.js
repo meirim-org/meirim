add8a6d25d37a5beba09fceeb287bf8fe1ba9260
TomerMe2 <tomer.laor1@gmail.com>
2020-08-18 01:48:55 +0300

    crawler works and mines chart 5

--- migrations/20190227202140_add_plan_status.js
+++ migrations/20190227202140_add_plan_status.js
@@ -1,18 +1,19 @@
+
 exports.up = function(knex, Promise) {
-  return knex.schema
-    .table('plan', table => {
-      table.string('status', 64);
-    })
-    .then(() =>
-      knex.schema.table('plan', table => {
-        table.index('status', 'status', 'index');
-      }),
-    )
-    .then(() => {
-      const sql = `update plan 
+    return knex.schema
+        .table('plan', table => {
+            table.string('status', 64)
+        })
+        .then(() =>
+            knex.schema.table('plan', table => {
+                table.index('status');
+            }),
+        )
+        .then(() => {
+            const sql = `update plan 
       set \`status\` = substring(\`data\`, locate('"STATION_DESC":',\`data\`)+17 , locate('"PL_AREA_DUNAM"',\`data\`)-locate('"STATION_DESC":',\`data\`)-20)`;
-      return knex.raw(sql);
-    });
+            return knex.raw(sql);
+        });
 };
 
 exports.down = function(knex, Promise) {};
