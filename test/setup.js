17b8d624b08d3a3572402f7508528062b0da94a4
Esraa Jbara <jbesraa@gmail.com>
2020-10-05 18:16:51 +0300

    encapsulation of database test utils

--- test/setup.js
+++ test/setup.js
@@ -1,102 +1,75 @@
 /* eslint-disable no-undef */
-const Promise = require('bluebird')
-const Config = require('../api/lib/config')
-const Knex = require('knex')
-const Bookshelf = require('bookshelf')
+const Promise = require('bluebird');
+const Config = require('../api/lib/config');
+const Knex = require('knex');
+const Bookshelf = require('bookshelf');
 // Bookshelf.plugin(require('../lib/bookshelf-mysql-gis'))
 
 const tables = [
-  'person',
+	'person',
 ];
 
 let connection;
 
 const database = {
 	 connection: null,
+
 	 initialize: function() {
+		if (connection != null){
+			 return connection;
+		}
 		const K = Knex({
  			client: Config.get('database.client'),
   		connection: Config.get('database.testconnection'),
   		debug: false
-		})
-		connection = Bookshelf(K)
+		});
+		connection = Bookshelf(K);
+		return connection;
 	 },
+
 	 truncate: function(tables) {
-
-  return Promise.each(tables, function (table) {
-		return connection.knex.schema.hasTable(table).then(function(exists) {
-			if(exists){
-				console.log(`dropping table ${table}`);
+		return Promise.each(tables, function (table) {
+			return connection.knex.schema.hasTable(table).then(function(exists) {
+				if(exists){
+					console.log(`dropping table ${table}`);
     		return connection.knex.schema.dropTable(table);
-			}
-		})
-  });
+				}
+			});
+		});
 	 },
+
 	 seed: function(tables) {
-
-  return Promise.each(tables, function (table) {
-		return connection.knex.schema.hasTable(table).then(async function(exists) {
-			if(!exists) {
+		return Promise.each(tables, function (table) {
+			return connection.knex.schema.hasTable(table).then(async function(exists) {
+				if(!exists) {
 					console.log(`creating table ${table}`);
 					await connection.knex.schema.createTable(table, function(t){
-					t.increments()
-					t.string('firstName', 255)
-				})
-				return connection.knex('person').insert({firstName: 'first'});
+						t.increments();
+						t.string('firstName', 255);
+					});
+					return connection.knex('person').insert({firstName: 'first'});
    		} 
-		})
-  });
-};
-	 }
-}
-
-function truncate (tables) {
-  return Promise.each(tables, function (table) {
-		return connection.knex.schema.hasTable(table).then(function(exists) {
-			if(exists){
-				console.log(`dropping table ${table}`);
-    		return connection.knex.schema.dropTable(table);
-			}
-		})
-  });
-};
-
-function seed (tables) {
-  return Promise.each(tables, function (table) {
-		return connection.knex.schema.hasTable(table).then(async function(exists) {
-			if(!exists) {
-					console.log(`creating table ${table}`);
-					await connection.knex.schema.createTable(table, function(t){
-					t.increments()
-					t.string('firstName', 255)
-				})
-				return connection.knex('person').insert({firstName: 'first'});
-   		} 
-		})
-  });
+			});
+		});
+	}
 };
 
 exports.mochaHooks = {
-  beforeAll() {
-		const K = Knex({
- 			client: Config.get('database.client'),
-  		connection: Config.get('database.testconnection'),
-  		debug: false
-		})
-		connection = Bookshelf(K)
+	beforeAll() {
+		database.initialize();
 	},
 
-  afterAll() {
-		console.log('afterAll')
-  },
+	afterAll() {
+		console.log('afterAll');
+	},
 
-  beforeEach() {
-		console.log('before each')
-		return truncate().then(seed);
-  },
+	beforeEach() {
+		console.log('before each');
+		return database.seed(tables);
+	},
 
-  afterEach() {
-		return truncate()
-  },
+	afterEach() {
+		return database.truncate(tables);
+	},
 
-}
+};
