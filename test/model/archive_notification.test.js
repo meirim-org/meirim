4956c4a61c1318c7abcb7ea4236d15ef022c5155
Esraa Jbara <jbesraa@gmail.com>
2020-10-07 10:22:01 +0300

    wip test for archive

--- test/model/archive_notification.test.js
+++ test/model/archive_notification.test.js
@@ -2,8 +2,9 @@ const assert = require('chai').assert;
 
 const ArchiveNotificationSeed = function(t) {
 	t.increments();
-	t.integer('plan_id'); // temp
+	t.integer('plan_id');
 	t.integer('person_id');
+	t.timestamps();
 };
 
 const getMockDatabase = () => {
@@ -15,13 +16,15 @@ const getMockDatabase = () => {
 	}
 	return mock;
 };
-const tables = [ 'archive_notification' ];
+
+const tables = ['archive_notification'];
 const seeders = [ArchiveNotificationSeed];
 
 describe('init', function() {
 
 	beforeEach(async function() {
 		const mockDatabase = getMockDatabase(); 
+		await mockDatabase.truncate(tables);
 		await mockDatabase.seed(tables, seeders);
 	});
 
@@ -31,20 +34,17 @@ describe('init', function() {
 	});
 
 	it('first', async function() {
-		const { ArchiveNotification } = require('../../api/model');
 		const {ArchiveNotificationController} = require('../../api/controller');
 		const req = {
 			session: {
 				person: 'fake'
 			},
 			body: {
-				person_id: '123',
-				plan_id: '123',
+				person_id: 123,
+				plan_id: 123,
 			}
 		};
-		const res = await ArchiveNotificationController.create(req);
-		
-		const n = new ArchiveNotification({a: '3'});
-		// assert.equal(n.rules.name, 'string');
+
+		 await ArchiveNotificationController.create(req);
 	});
 });
