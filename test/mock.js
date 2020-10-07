a8827d1f8db8530a348c5da332948e59206d9ffe
Esraa Jbara <jbesraa@gmail.com>
2020-10-07 16:10:39 +0300

    refactor module and add insertData fn

--- test/mock.js
+++ test/mock.js
@@ -1,44 +1,89 @@
 const Promise = require('bluebird');
+const structs = require('./tables_structs');
+
+const tableActions = {
+	createTable: async function(knexClient, tableName) {
+		return await knexClient.schema.createTable(tableName, function(t){
+			console.log('structs', structs);
+			const table = structs[tableName](t);
+			console.log(`created table ${tableName}`);
+			return table;
+		}); 
+	},
+
+	isExist: async function(knexClient, tableName) {
+		return await knexClient.schema.hasTable(tableName).then(function(exists){
+			console.log('exists', exists);
+			return exists;});
+	},
+
+	dropTable: async function(knexClient, tableName) {
+		return await knexClient.schema.dropTable(tableName);
+	},
+
+	insertDataToTable: async function(knexClient, tableName, tableData) {
+		return tableData.map(async function(data) {
+			return await knexClient(tableName).insert(data);
+		});
+	},
+};
+
 
 const mockDatabase = {
-	 connection: null,
+	connection: null,
+	knexClient: null,
 
-	 initialize: function(clientConnection) {
+	initialize: function(clientConnection) {
 		if (this.connection != null){
 			 return this.connection;
 		}
-
 		this.connection = clientConnection;
+		this.knexClient = clientConnection.knex;
+		console.log('DATABASE CONNECTION CONFIG', this.knexClient.context.client.config);
 
-		console.log('DATABASE CONNECTION CONFIG', this.connection.knex.context.client.config);
 		return this.connection;
-	 },
+	},
 
-	 truncate: function(tables) {
-		const connection = this.connection;
-		return Promise.each(tables, function (table) {
-			return connection.knex.schema.hasTable(table).then(function(exists) {
-				if(exists){
-					console.log(`dropping table ${table}`);
-    			return connection.knex.schema.dropTable(table);
-				}
-			});
+	dropTables: function(tables) {
+		const knexClient = this.knexClient;
+
+		return Promise.each(tables, async function (tableName) {
+			const isTableExist = await tableActions.isExist(knexClient, tableName);
+			if(isTableExist){
+				await tableActions.dropTable(knexClient, tableName);
+			}
+			return;
 		});
-	 },
+	},
 
-	 seed: function(tables, seeders) {
-		const connection = this.connection;
-		return Promise.each(tables, function (table, index) {
-			return connection.knex.schema.hasTable(table).then(async function(exists) {
-				if(!exists) {
-					console.log(`creating table ${table}`);
-					await connection.knex.schema.createTable(table, function(t){
-						const seeder = seeders[index];
-						seeder(t);
-					});
-					// return connection.knex('person').insert({firstName: 'first'});
-   		} 
-			});
+	createTables: function(tables) {
+		const knexClient = this.knexClient;
+
+		return Promise.each(tables, async function (tableName) {
+			const isExist = await tableActions.isExist(knexClient, tableName);
+			if(!isExist) {
+				await tableActions.createTable(knexClient, tableName);
+				return;
+			} else {
+				console.log(`table ${tableName} already exist`);
+				return;
+			}
+		});
+	},
+
+	insertData: function(tables, data) {
+		const knexClient = this.knexClient;
+
+		return Promise.each(tables, async function(tableName) {
+			const isTableExist = await tableActions.isExist(knexClient, tableName);
+			if(!isTableExist) {
+				await tableActions.createTable(knexClient, tableName); 
+			}
+			
+			const tableData = data[tableName];
+
+			tableActions.insertDataToTable(knexClient, tableName, tableData);
+			return;
 		});
 	}
 };
