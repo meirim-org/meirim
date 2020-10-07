870dc6af2b816b8f392ae92774fdc3f0a1ec9908
Esraa Jbara <jbesraa@gmail.com>
2020-10-07 19:48:50 +0300

    rm unused file

--- test/model/notification.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert
-
-describe('init', function() {
-	it('first', function() {
-		const { Notification } = require('../../api/model');
-		const n = new Notification();
-		assert.equal(n.rules.name, 'string')
-	});
-});
