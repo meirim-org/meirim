0dcb64f4c8f1779b7b2bbd200b8293f6d508ddd1
Esraa Jbara <jbesraa@gmail.com>
2020-10-08 17:26:40 +0300

    basic test for plan model

---
+++ test/model/plan.test.js
@@ -0,0 +1,42 @@
+const expect = require('chai').expect;
+const { Plan } = require('../../api/model');
+
+describe('Plan model', function() {
+	let instance;
+	beforeEach(function () {
+		instance = new Plan();
+	}); 
+	
+	afterEach(function() {
+		instance = null;
+	});
+
+	it('has the right rules', function() {
+		const rules = instance.rules;
+		expect(rules.sent).to.eql('integer');
+		expect(rules.OBJECTID).to.eql(['required', 'integer']);
+		expect(rules.rating).to.eql(['required', 'number']);
+		expect(rules.PL_NUMBER).to.eql('string');
+		expect(rules.jurisdiction).to.eql('string');
+		expect(rules.areaChanges).to.eql('string');
+		expect(rules.PL_NAME).to.eql('string');
+		expect(rules.data).to.eql([ 'required' ]);
+		expect(rules.geom).to.eql([ 'required', 'object' ]);
+		expect(rules.PLAN_COUNTY_NAME).to.eql('string');
+	});
+
+	it('has the right table name', function() {
+		const tableName = instance.tableName;
+		expect(tableName).to.eql('plan');
+	});
+
+	it('has the right defaults', function() {
+		const defaults = instance.defaults();
+		expect(defaults).to.eql({sent: 0});
+	});
+
+	it('has timestamps', function() {
+		const isTimestamps = instance.hasTimestamps;
+		expect(isTimestamps).to.eql(true);
+	});
+});
