882b89cdf301bc5048b7d7889f7c24b98f28fa3f
Esraa Jbara <jbesraa@gmail.com>
2020-10-08 17:13:57 +0300

    add relevant files

--- api/controller/index.js
+++ api/controller/index.js
@@ -1,7 +1,9 @@
 const ArchiveNotificationController = require('./archive_notification');
 const NotificationController = require('./notification');
+const PlanController = require('./plan');
 
 module.exports = {
 	ArchiveNotificationController,
-	NotificationController
+	NotificationController,
+	PlanController
 };
--- test/tables_structs/index.js
+++ test/tables_structs/index.js
@@ -1,2 +1,3 @@
 exports.archive_notification = require('./archive_notification_struct');
 exports.notification = require('./notification_struct');
+exports.plan = require('./plan_struct');
