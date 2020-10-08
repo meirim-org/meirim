20053b820221ee829a92f27b4244885cdd3940c2
Esraa Jbara <jbesraa@gmail.com>
2020-10-08 16:27:23 +0300

    add notification table

--- api/model/notification.js
+++ api/model/notification.js
@@ -1,10 +1,36 @@
 const Model = require('./base_model');
+const Exception = require('./exception');
 
 class Notification extends Model {
+
 	get rules() {
 		return {
-			name: 'string',
+			person_id: ['required', 'integer'],
+			plan_id: ['required', 'integer'],
+			seen: 'boolean',
+			type: ['required', 'string']
 		};
+	}	
+
+	get tableName() {
+		return 'notification';
+	}
+
+	get hasTimestamps() {
+		return true;
+	}
+
+	defaults() {
+		return {
+			seen: false 
+		};
+	}
+
+	static canCreate(session) {
+		if (!session.person) {
+			throw new Exception.NotAllowed('Must be logged in');
+		}
+		return Promise.resolve(this);
 	}
 }
 
--- test/tables_structs/index.js
+++ test/tables_structs/index.js
@@ -1,1 +1,2 @@
 exports.archive_notification = require('./archive_notification_struct');
+exports.notification = require('./notification_struct');
