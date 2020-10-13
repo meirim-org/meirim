ce2461dbd4040af509a1d83342e0ecb3548bd6cd
Esraa Jbara <jbesraa@gmail.com>
2020-10-13 20:21:14 +0300

    fix crawler test

--- test/tables_structs/plan_struct.js
+++ test/tables_structs/plan_struct.js
@@ -2,14 +2,16 @@ const PlanStruct = function(table) {
 	table.increments();
 	table.integer('sent');
 	table.integer('OBJECTID');
+	table.string('goals_from_mavat');
+	table.string('main_details_from_mavat');
 	table.string('PLAN_COUNTY_NAME');
 	table.string('PL_NUMBER');
 	table.string('PL_NAME');
-	table.string('data');
+	table.text('data', 65535);
 	table.string('PLAN_CHARACTOR_NAME');
 	table.string('plan_url');
 	table.string('status');
-	table.string('geom');
+	table.binary('geom');
 	table.string('jurisdiction');
 	table.string('areaChanges');
 	table.integer('rating');
