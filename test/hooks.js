1acd8f1dac3c936ac4a0480cea7d9d2ec96ea328
Esraa Jbara <jbesraa@gmail.com>
2020-10-13 19:18:53 +0300

    fix test for sign controller

--- test/hooks.js
+++ test/hooks.js
@@ -1,40 +1,7 @@
-const mockRequire = require('mock-require');
-const importFresh = require('import-fresh');
-
 exports.mochaHooks = {
-  beforeAll() {
-    // override database connection config values so we
-    // access our test database. override smtp config
-    // values so we never accidentally send emails while
-    // testing
-    process.env.NODE_CONFIG = JSON.stringify({
-      database: {
-        connection: {
-          host: 'localhost',
-          port: '33060',
-          user: 'root',
-          password: 'meirim_test',
-          database: 'meirim_test'
-        }
-      },
-      email: {
-        options: {
-          host: 'smtp.example.com',
-          port: 465,
-          secure: true,
-          auth: {
-            user: 'user',
-            pass: 'pass'
-          }
-        }
-      }
-    });
-    
-    const testConfig = importFresh('config');
-    mockRequire('config', testConfig);
-  },
+	beforeAll() {
+	},
   
-  afterAll() {
-    mockRequire.stop('config');
-  },  
+	afterAll() {
+	},  
 };
