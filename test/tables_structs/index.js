e0bfb9697c2f8ba2b1b51ff3075968b2864b42ef
Esraa Jbara <jbesraa@gmail.com>
2020-10-13 15:45:26 +0300

    test person model

--- test/tables_structs/index.js
+++ test/tables_structs/index.js
@@ -1,3 +1,4 @@
 exports.archive_notification = require('./archive_notification_struct');
 exports.notification = require('./notification_struct');
 exports.plan = require('./plan_struct');
+exports.person = require('./person_struct');
