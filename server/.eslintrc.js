6be61d2220cdfd768c979c2749a3661959656a83
Esraa Jbara <jbesraa@gmail.com>
2020-10-20 14:13:38 +0300

    install eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'node': true,
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018
+	},
+	'rules': {
+		"camelcase": "off",
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+	}
+};
