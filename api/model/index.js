c0b4cdf20cc1d302f3951e13f549fe27cf39982b
nadavmoreno <>
2020-10-21 16:10:15 +0300

    wip

--- api/model/index.js
+++ api/model/index.js
@@ -2,10 +2,13 @@ const Notification = require('./notification');
 const ArchiveNotification = require('./archive_notification');
 const Plan = require('./plan');
 const Person = require('./person');
+const Alert = require('./alert');
+
 
 module.exports = {
 	ArchiveNotification,
 	Notification,
 	Plan,
-	Person
+	Person,
+	Alert
 };
---
+++ test/model/alert.test.js
@@ -0,0 +1,39 @@
+const expect = require('chai').expect;
+const { mockDatabase } = require('../mock');
+
+const { Alert } = require('../../api/model');
+
+describe('Alert model', function() {
+	let instance;
+	beforeEach(function () {
+		instance = new Alert();
+	});
+
+	afterEach(function() {
+		instance = null;
+	});
+
+	it('has the right rules', function() {
+		const rules = instance.rules;
+		expect(rules.person_id).to.eql(['required', 'integer']);
+		expect(rules.address).to.eql(['required', 'string']);
+		expect(rules.geom).to.eql(['required', 'object']);
+		expect(rules.radius).to.eql(['required', 'number' ]);
+	});
+
+	it('has the right table name', function() {
+		const tableName = instance.tableName;
+		expect(tableName).to.eql('alert');
+	});
+
+	it('has the right defaults', function() {
+		const defaults = instance.defaults();
+		expect(defaults).to.eql({radius: 5});
+	});
+
+	it('has the right geometry', function() {
+		const defaults = instance.geometry;
+		expect(defaults).to.eql(['geom']);
+	});
+
+});
