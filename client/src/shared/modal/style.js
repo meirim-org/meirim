c3f66212bfdb98310858970c5a8688b423044b45
Esraa Jbara <jbesraa@gmail.com>
2020-11-04 10:12:54 +0200

    add global modal

---
+++ client/src/shared/modal/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const ModalWrapper = styled.div`
+	padding-top: 0.5em;
+	padding-right: 0.5em;
+`
+export const ModalContentWRapper = styled.div`
+	display:flex;
+	width: 504;
+	flex-direction: column;
+	height: 656;
+	background-color: #ffffff;
+	border-radius: 15px 15px;
+`
+
+export const IconWrapper = styled.div`
+	padding-top: 0.5em;
+	padding-right: 0.5em;
+`
