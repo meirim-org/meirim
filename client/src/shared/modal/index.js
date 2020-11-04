cbb933b9d3fa1858a4fccf9d003a6917ac32d0ac
Esraa Jbara <jbesraa@gmail.com>
2020-11-04 10:12:54 +0200

    add global modal

---
+++ client/src/shared/modal/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Modal from "@material-ui/core/Modal"
+import CloseIcon from "@material-ui/icons/Close"
+import * as SC from "./style"
+
+const useStyles = makeStyles(() => ({
+	modal: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+}))
+
+const MainModal = ({children}) => {
+	const classes = useStyles()
+
+	return (
+		<SC.ModalWrapper>
+			<Modal
+				open
+				aria-labelledby="server-modal-title"
+				aria-describedby="server-modal-description"
+				className={classes.modal}
+			>
+				<SC.ModalContentWRapper>
+					<SC.IconWrapper>
+						<CloseIcon style={{cursor: "pointer"}}/>
+					</SC.IconWrapper>
+					{children}
+				</SC.ModalContentWRapper>
+			</Modal>
+		</SC.ModalWrapper>
+	)
+}
+
+MainModal.propTypes = {
+	children: React.element
+}
+
+
+export default MainModal
