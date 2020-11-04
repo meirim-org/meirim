98be60c1c2be20eb63bb8a7dcbc3e953c94f1dcc
Esraa Jbara <jbesraa@gmail.com>
2020-11-04 10:13:07 +0200

    add global input

---
+++ client/src/shared/input/index.js
@@ -0,0 +1,77 @@
+import React from "react"
+import TextField from "@material-ui/core/TextField"
+import { makeStyles } from "@material-ui/core/styles"
+import PropTypes from "prop-types"
+import * as SC from "./style"
+
+const useStyles = makeStyles(() => ({
+	input: {
+		backgroundColor: "white",
+		height: "44px",
+		borderRadius: "12px",
+		border: "none"
+	},
+	adornedEnd: {
+		height: "40px"
+	}
+}))
+
+const TextInput = ({name, variant = "outlined", type, label, required = false, size = "small"}) => {
+	const classes = useStyles()
+
+	return (
+		<>
+			{label && <SC.Label> {label} {required && <SC.Star>*</SC.Star>} </SC.Label>}
+			<TextField
+				name={name}
+				variant={variant}
+				type={type}
+				size={size}
+				InputProps={{
+					classes: classes.input
+				}}
+			/>
+		</>
+	)
+}
+
+TextInput.propTypes = {
+	name: PropTypes.string.isRequired,
+	type: PropTypes.string.isRequired,
+	size: PropTypes.string,
+	required: PropTypes.bool,
+	label: PropTypes.string,
+	variant: PropTypes.string,
+
+}
+
+export default TextInput
+
+// const [showPassword, setShowPassword] = useState(true)
+
+// InputProps={{
+// 	endAdornment:
+// 					<IconButton
+// 						aria-label="toggle password visibility"
+// 						onClick={handleClickShowPassword}
+// 						onMouseDown={handleMouseDownPassword}
+// 					>
+// 						{showPassword ? <Visibility /> : <VisibilityOff />}
+// 					</IconButton>,
+// 	classes: {
+// 		adornedEnd: classes.adornedEnd,
+// 		root: classes.input
+// 	}
+// }}
+
+// import IconButton from "@material-ui/core/IconButton"
+// import Visibility from "@material-ui/icons/Visibility"
+// import VisibilityOff from "@material-ui/icons/VisibilityOff"
+
+// const handleClickShowPassword = () => {
+// 	setShowPassword(!showPassword)
+// }
+
+// const handleMouseDownPassword = (event) => {
+// 	event.preventDefault()
+// }
---
+++ client/src/shared/input/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Star = styled.span`
+  color:red;
+`
+
+export const Label = styled.div`
+  font-family: Assistant;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  text-align: right;
+  color: #665d71;
+`
