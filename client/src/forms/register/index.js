7b02820363d8f848c83d187ab7c70a11165db70e
Esraa Jbara <jbesraa@gmail.com>
2020-11-08 11:07:39 +0200

    not final

--- client/src/forms/register/index.js
+++ client/src/forms/register/index.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { Modal, TextInput, Button } from "../../shared"
+import { Modal,Dropdown, TextInput, TextArea, Button } from "../../shared"
 import * as SC from "./style"
 
-const RegisterForm = () => {
+export const RegisterForm = () => {
 	return (
 		<Modal>
 			<SC.MainWrapper>
@@ -16,13 +16,13 @@ const RegisterForm = () => {
 				<SC.InputsWrapper>
 					<SC.InputsTitle>הרשמה למעירים</SC.InputsTitle>
 					<SC.InputWrapper>
-						<TextInput name="name" label="שם מלא" type="text"/>
+						<TextInput name="name" label="שם מלא" type="text" required={true}/>
 					</SC.InputWrapper>
 					<SC.InputWrapper>
-						<TextInput name="email" label="אימייל" type="email"/>
+						<TextInput name="email" label="אימייל" type="email" required={true}/>
 					</SC.InputWrapper>
 					<SC.InputWrapper>
-						<TextInput name="password" label="סיסמא" type="password"/>
+						<TextInput name="password" label="סיסמא" type="password" required={true}/>
 					</SC.InputWrapper>
 				</SC.InputsWrapper>
 				<SC.ButtonWrapper>
@@ -33,4 +33,35 @@ const RegisterForm = () => {
 	)
 }
 
-export default RegisterForm
+export const UserInfoForm = () => {
+	const dropDownOptions =  [
+		{
+			value: 1,
+			text:" תושב/ת שכאפת לו/ה"
+		}
+	]
+
+	return ( 
+		<Modal>
+			<SC.MainWrapper>
+				<SC.Titles>
+					<SC.Title>בואו להיות חלק מקהילת מעירים!</SC.Title>
+				</SC.Titles>
+				<SC.InputsWrapper>
+					<SC.InputWrapper>
+						<TextInput name="name" label="כתובת" type="text" helperText="כדי לקבל עדכונים על מה בונים לך ליד הבית"/>
+					</SC.InputWrapper>
+					<SC.InputWrapper>
+						<Dropdown options={dropDownOptions} required={true} label="מי אני"/>
+					</SC.InputWrapper>
+					<SC.InputWrapper>
+						<TextArea helperText="כדי ששאר חברי הקהילה יכירו אותך" label="קצת עליך"/>
+					</SC.InputWrapper>
+				</SC.InputsWrapper>
+				<SC.ButtonWrapper>
+					<Button text="המשך"/>
+				</SC.ButtonWrapper>
+			</SC.MainWrapper>
+		</Modal>
+	)
+}
