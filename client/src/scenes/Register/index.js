d58f833a576c2a36c29f4775476809c203e05273
Esraa Jbara <jbesraa@gmail.com>
2020-11-08 18:10:30 +0200

    add register scene

---
+++ client/src/scenes/Register/index.js
@@ -0,0 +1,5 @@
+import Signup from "./signup"
+
+export {
+	Signup
+}
---
+++ client/src/scenes/Register/signup/style.js
@@ -0,0 +1,86 @@
+import styled from "styled-components"
+
+export const Titles = styled.div`
+  padding-bottom: 2em;
+  display: grid;
+`
+
+export const Title = styled.h1`
+	font-size: 1.8em;
+  text-align: center;
+  font-family: Assistant;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+  color: #29244a;
+`
+
+export const SubTitle = styled.h1`
+  font-family: Assistant;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  letter-spacing: normal;
+  line-height: 1.5;
+  text-align: center;
+  color: #000000;
+`
+
+export const SubTitleWrapper = styled.div`
+`
+
+export const InputsWrapper = styled.div`
+  display: grid;
+  background-color: #f1eef2;
+  padding-top: 1em;
+`
+
+export const MainWrapper = styled.div`
+  display: grid;
+`
+
+export const InputWrapper = styled.div`
+  padding: 1em 4em 0 4em;
+  display: flex;
+  flex-direction: column;
+`
+
+export const InputsTitle = styled.div`
+  font-family: Assistant;
+  font-size: 20px;
+  font-weight: normal;
+  padding-top: 1em;
+  padding-bottom: 0;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  text-align: center;
+  color: #3e385c;
+`
+
+export const ButtonWrapper = styled.div`
+  border-radius:0 0 12px 12px;
+  background-color: #f1eef2;
+  display: grid;
+  padding: 2.5em 4em 2em 4em;
+`
+
+export const Label = styled.div`
+  font-family: Assistant;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  text-align: right;
+  color: #665d71;
+`
+
+export const Star = styled.span`
+  color:red;
+`
