d00b42b35733be655f2fefb545429eea775141e2
Esraa Jbara <jbesraa@gmail.com>
2020-11-14 15:54:20 +0200

    add fullpage to useeffect vars

--- client/src/components/Wrapper/Wrapper.jsx
+++ client/src/components/Wrapper/Wrapper.jsx
@@ -1,40 +1,41 @@
-import React, {useRef, useEffect, useState, cloneElement} from "react";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import React, {useRef, useEffect, useState, cloneElement} from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-import Navigation from "../Navigation";
-import Footer from "../Footer";
+import Navigation from '../Navigation';
+import Footer from '../Footer';
 
 export default function Wrapper(props) {
-    const {me, children, fullPage} = props;
-    const parentRef = useRef(null);
-    const [navHeight, setNavHeight] = useState(0);
+	const {me, children, fullPage} = props;
+	const parentRef = useRef(null);
+	const [navHeight, setNavHeight] = useState(0);
 
-    useEffect(() => {
-        if (parentRef.current && fullPage) {
-            setTimeout(() => setNavHeight(parentRef.current.clientHeight), 1);
-        }
-    }, []);
+	useEffect(() => {
+		if (parentRef.current && fullPage) {
+			setTimeout(() => setNavHeight(parentRef.current.clientHeight), 1);
+		}
+	}, [fullPage]);
 
-    const renderChildren = () => {
-        if (!children) {
-            return <div style={{textAlign: "center"}}>
-                <CircularProgress/>
-            </div>
-        } else {
-            if (navHeight) {
-                return cloneElement(children, {navHeight})
-            }
-        }
-        return children
-    }
+	const renderChildren = () => {
+		if (!children) {
+			return <div style={{textAlign: 'center'}}>
+				<CircularProgress/>
+			</div>
+		} else {
+			if (navHeight) {
+				return cloneElement(children, {navHeight})
+			}
+		}
+		
+		return children
+	}
 
-    return (
-        <>
-            <div ref={parentRef}>
-                <Navigation me={me}/>
-            </div>
-            {renderChildren()}
-            <Footer/>
-        </>
-    );
+	return (
+		<>
+			<div ref={parentRef}>
+				<Navigation me={me}/>
+			</div>
+			{renderChildren()}
+			<Footer/>
+		</>
+	);
 }
