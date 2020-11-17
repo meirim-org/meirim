29a4b303e8861ba6ff278aa2be9ad18a376811e4
Esraa Jbara <jbesraa@gmail.com>
2020-11-17 21:10:00 +0200

    small files refactor

--- client/src/router/contants.js
+++ client/src/router/contants.js
@@ -1,1 +1,3 @@
 export const EMAIL_SENT_PAGE = '/email-sent'
+export const SIGN_UP = '/sign/up'
+export const AUTHENTICATE_EMAIL = '/sign/auth/email'
---
+++ client/src/scenes/Register/validations.js
@@ -0,0 +1,16 @@
+
+export const formValidation = ({ name, email, password, dirtyInputs, onFocusInput }) => {
+	const isValidEmail = onFocusInput.email  || Boolean(email)? true : !dirtyInputs.email
+	const isValidName = onFocusInput.name  || Boolean(name) ? true : !dirtyInputs.name
+	const isValidPassword = onFocusInput.password  || password.length >= 6 ? true : !dirtyInputs.password
+
+	return { isValidEmail, isValidName, isValidPassword }
+}
+
+export const getFormErrors = ({ isValidEmail, isValidName, isValidPassword }) => {
+	const emailError = { isValid: isValidEmail, message: isValidEmail? '' : 'שדה חובה' }
+	const nameError = { isValid: isValidName, message: isValidName ? '' : 'שדה חובה' }
+	const passwordError = { isValid: isValidPassword, message: isValidPassword ? '' : 'לפחות ששה תווים' }
+
+	return { emailError, nameError, passwordError }
+}
