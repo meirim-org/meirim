982847bbc25a92719fbd75d728d96f263a349f80
Esraa Jbara <jbesraa@gmail.com>
2020-11-18 21:05:59 +0200

    add migrations

---
+++ server/migrations/20201811204800_move_values_from_alias_to_name.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+	try { 
+	const updateQuery = `UPDATE person SET name = alias`
+	return knex.raw(updateQuery)
+	} catch(err) {
+		console.log('err in migration', err)
+	}
+};
+
+exports.down = function() {
+};
