d693f64e08a2cd39fdfc9e503c29a819a3d03404
gruppin <gruppin@gmail.com>
2020-11-19 17:36:13 +0200

    tree permit v1
    trees model + read regional file and save
    
    3changed xls parsing method

---
+++ server/api/model/tree_permit.js
@@ -0,0 +1,41 @@
+const Model = require('./base_model');
+const tpc = require('./tree_permit_constants');
+class TreePermit extends Model {
+
+	get rules() {
+		return {
+
+			[tpc.REGIONAL_OFFICE]: 'string',
+			[tpc.PERMIT_NUMBER]: 'string',
+			[tpc.ACTION]: 'string', // cutting , copying
+			[tpc.PERMIT_ISSUE_DATE]: 'string',
+			[tpc.PERSON_REQUEST_NAME]: 'string',
+			[tpc.START_DATE]: 'string',
+			[tpc.END_DATE]: 'string',
+			[tpc.LAST_DATE]: 'string',
+			[tpc.APPROVER_NAME]: 'string',
+			[tpc.APPROVER_TITLE]: 'string',
+
+			// Location
+			[tpc.PLACE]: 'string',
+			[tpc.STREET]: 'string',
+			[tpc.STREET_NUMBER]: 'string',
+			[tpc.GUSH]: 'string',
+			[tpc.HELKA]: 'string',
+			
+			// Trees details
+			[tpc.TREE_NAME]: 'string',
+			[tpc.TREE_KIND]: 'string',
+			[tpc.NUMBER_OF_TREES]: 'integer',
+			[tpc.REASON_SHORT]: 'string',
+			[tpc.REASON_DETAILED]: 'string',
+			[tpc.COMMENTS_IN_DOC]: 'string'
+		};
+	}
+
+	get tableName () {
+		return 'tree_permit';
+	}
+}
+
+module.exports = TreePermit;
