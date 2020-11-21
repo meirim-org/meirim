7479ca11298656f74cca7127f6c905023ae5bc67
nadavmoreno <>
2020-11-21 17:13:37 +0200

    icon added

--- client/src/shared/index.js
+++ client/src/shared/index.js
@@ -5,7 +5,9 @@ import Dropdown from './dropdown';
 import TextArea from './textArea';
 import Link from './link';
 import Navigation from './navigation';
+import Row from './row';
+import Icon from './icon';
 
 export {
-	Button, Dropdown, TextArea, TextInput, Modal, Link, Navigation
+	Button, Dropdown, TextArea, TextInput, Modal, Link, Navigation, Row, Icon
 };
