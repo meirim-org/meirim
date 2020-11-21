ca85611f98b0d8477badb6be3a63b25d3ead8958
Esraa Jbara <jbesraa@gmail.com>
2020-11-21 13:09:43 +0200

    validate on client s

--- client/src/scenes/Register/validations.js
+++ client/src/scenes/Register/validations.js
@@ -1,5 +1,5 @@
 export const firstStepValidation = ({ name, email, password }) => {
-	const isValidEmail =  Boolean(email)
+	const isValidEmail =  email ? validateEmail(email) : false
 	const isValidName =  Boolean(name)
 	const isValidPassword = password.length >= 6 
 
@@ -7,17 +7,43 @@ export const firstStepValidation = ({ name, email, password }) => {
 }
 
 export const formValidation = ({ name, email, password, dirtyInputs, onFocusInput }) => {
-	const isValidEmail = onFocusInput.email  || Boolean(email)? true : !dirtyInputs.email
-	const isValidName = onFocusInput.name  || Boolean(name) ? true : !dirtyInputs.name
-	const isValidPassword = onFocusInput.password  || password.length >= 6 ? true : !dirtyInputs.password
+	const isValidEmail = 
+		onFocusInput.email  || Boolean(email)? true : !dirtyInputs.email
+	const isValidName = 
+		onFocusInput.name  || Boolean(name) ? true : !dirtyInputs.name
+	const isValidPassword = 
+		onFocusInput.password  || password.length >= 6 ? true : !dirtyInputs.password
 
 	return { isValidEmail, isValidName, isValidPassword }
 }
 
-export const getFormErrors = ({ isValidEmail, isValidName, isValidPassword }) => {
-	const emailError = { isValid: isValidEmail, message: isValidEmail? '' : 'שדה חובה' }
-	const nameError = { isValid: isValidName, message: isValidName ? '' : 'שדה חובה' }
-	const passwordError = { isValid: isValidPassword, message: isValidPassword ? '' : 'לפחות ששה תווים' }
+const inValidEmailMessage = 'מייל לא תקין'
+const emptyInputMessage = 'שדה חובה'
+const shortPasswordMessage = 'לפחות ששה תווים'
+
+export const getFormErrors = ({ 
+	validations: { isValidEmail, isValidName, isValidPassword }, 
+	values: { email, password } }) => {
+	const emailError = { 
+		isValid: isValidEmail,
+		message: isValidEmail ? '' : email ? inValidEmailMessage : emptyInputMessage
+	}
+	const nameError = { 
+		isValid: isValidName, 
+		message: isValidName ? '' : 'שדה חובה' 
+	}
+	const passwordError = { 
+		isValid: isValidPassword, 
+		message: isValidPassword ? '' : password ? shortPasswordMessage : emptyInputMessage
+	}
 
 	return { emailError, nameError, passwordError }
 }
+
+const validateEmail = (email) => {
+	const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	
+	return re.test(email);
+}
+
+
