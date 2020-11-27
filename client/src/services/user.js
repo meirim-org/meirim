eb66f5c04bb1dadc812df03d502dc4650d5a3e41
Esraa Jbara <jbesraa@gmail.com>
2020-11-27 19:42:19 +0200

    add user service

---
+++ client/src/services/user.js
@@ -0,0 +1,11 @@
+import api from './api'
+
+export const logout = async () => {
+	try {
+		const response = await api.post('/sign/out')
+		
+		return response
+	} catch (err){
+		throw new err
+	}
+}
