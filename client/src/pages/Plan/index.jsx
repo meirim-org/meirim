852d9e4d02ffd9121a68f9fb8c0c9de5259aecb4
nadavmoreno <nadav@soficoop.com>
2020-11-28 02:57:09 +0200

    plan preliminary

---
+++ client/src/pages/Plan/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import React, { useState, useRef, useEffect } from 'react';
+import * as SC from './style';
+
+const Plan = ({ ...props }) => {
+
+	return (
+	        <div>nnn</div>
+	);
+};
+
+export default Plan;
