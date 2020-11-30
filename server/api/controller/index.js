f3d182217734384fc0dc53022bbcc65075512a19
Esraa Jbara <jbesraa@gmail.com>
2020-11-30 22:28:20 +0200

    add controller to index

--- server/api/controller/index.js
+++ server/api/controller/index.js
@@ -1,9 +1,11 @@
 const ArchiveNotificationController = require('./archive_notification');
 const NotificationController = require('./notification');
+const CommentController = require('./comment');
 const PlanController = require('./plan');
 const AlertController = require('./alert');
 
 module.exports = {
+	CommentController,
 	AlertController,
 	ArchiveNotificationController,
 	NotificationController,
