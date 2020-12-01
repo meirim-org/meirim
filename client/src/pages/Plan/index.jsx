84cdf25201b190d6494825447f26d25138788f62
nadavmoreno <nadav@soficoop.com>
2020-12-01 11:54:06 +0200

    wrapper add

--- client/src/pages/Plan/index.jsx
+++ client/src/pages/Plan/index.jsx
@@ -9,6 +9,7 @@ import StarBorderIcon from '@material-ui/icons/StarBorder';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import { TabPanel, Typography } from '../../shared'
 import { useTheme } from '@material-ui/styles';
+import Wrapper from 'components/Wrapper';
 
 function a11yProps(index) {
 	return {
@@ -18,8 +19,8 @@ function a11yProps(index) {
 }
 
 const Plan = () => {
-    const theme = useTheme();
-    const [value, setValue] = React.useState(0);
+	const theme = useTheme();
+	const [value, setValue] = React.useState(0);
 
 
 	const handleChange = (event, newValue) => {
@@ -27,72 +28,74 @@ const Plan = () => {
 	};
 
 	return (
-		<SC.MainWrapper>
-			<SC.Content>
-				<SC.Header>
-					<SC.TitlesAndTabs>
-						<SC.SubTitleWrapper>
-							<Typography variant="planDetailTitle" mobileVariant="smallTitle" component="span" color={theme.palette.primary.main}>
+	    <Wrapper>
+			<SC.MainWrapper>
+				<SC.Content>
+					<SC.Header>
+						<SC.TitlesAndTabs>
+							<SC.SubTitleWrapper>
+								<Typography variant="planDetailTitle" mobileVariant="smallTitle" component="span" color={theme.palette.primary.main}>
                                 ירושלים
-							</Typography>
-						</SC.SubTitleWrapper>
-						<SC.TitleWrapper>
-							<Typography variant="planTitle" mobileVariant="paragraphText" component="h1" color={theme.palette.black}>
+								</Typography>
+							</SC.SubTitleWrapper>
+							<SC.TitleWrapper>
+								<Typography variant="planTitle" mobileVariant="paragraphText" component="h1" color={theme.palette.black}>
                                 איחוד וחלוקה לחלקות 18,17,16,9 ברחוב התעשייה, שכונת תלפיות, ירושלים
-							</Typography>
-						</SC.TitleWrapper>
-						<SC.AppBar position="static">
-							<Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-								<Tab label={t.summary} {...a11yProps(0)} />
-								<Tab label={
+								</Typography>
+							</SC.TitleWrapper>
+							<SC.AppBar position="static">
+								<Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+									<Tab label={t.summary} {...a11yProps(0)} />
+									<Tab label={
 								    <Badge
-										badgeContent={'12'}
-									>
-										{t.opinion}
-									</Badge>
-								} {...a11yProps(1)} />
-								<Tab label={t.planningInformation} {...a11yProps(2)} />
-							</Tabs>
-						</SC.AppBar>
-					</SC.TitlesAndTabs>
-					<SC.Buttons>
-						<Button
-							variant="contained"
-							color="primary"
-							startIcon={<ShareIcon />}
-						>
-							<span>{t.sharing}</span>
-						</Button>
-						<Button
-							variant="contained"
-							color="primary"
-							startIcon={<StarBorderIcon />}
-						>
-							<span>{t.saving}</span>
-						</Button>
-						<Button
-							variant="contained"
-							color="primary"
-							startIcon={<ChatBubbleOutlineIcon />}
-						>
-							<span>{t.addAnOpinion}</span>
-						</Button>
-					</SC.Buttons>
-				</SC.Header>
-				<SC.Main>
-					<TabPanel value={value} index={0}>
+											badgeContent={'12'}
+										>
+											{t.opinion}
+										</Badge>
+									} {...a11yProps(1)} />
+									<Tab label={t.planningInformation} {...a11yProps(2)} />
+								</Tabs>
+							</SC.AppBar>
+						</SC.TitlesAndTabs>
+						<SC.Buttons>
+							<Button
+								variant="contained"
+								color="primary"
+								startIcon={<ShareIcon />}
+							>
+								<span>{t.sharing}</span>
+							</Button>
+							<Button
+								variant="contained"
+								color="primary"
+								startIcon={<StarBorderIcon />}
+							>
+								<span>{t.saving}</span>
+							</Button>
+							<Button
+								variant="contained"
+								color="primary"
+								startIcon={<ChatBubbleOutlineIcon />}
+							>
+								<span>{t.addAnOpinion}</span>
+							</Button>
+						</SC.Buttons>
+					</SC.Header>
+					<SC.Main>
+						<TabPanel value={value} index={0}>
                         Item One
-					</TabPanel>
-					<TabPanel value={value} index={1}>
+						</TabPanel>
+						<TabPanel value={value} index={1}>
                         Item Two
-					</TabPanel>
-					<TabPanel value={value} index={2}>
+						</TabPanel>
+						<TabPanel value={value} index={2}>
                         Item Three
-					</TabPanel>
-				</SC.Main>
-			</SC.Content>
-			<div>map</div>
-		</SC.MainWrapper>
+						</TabPanel>
+					</SC.Main>
+				</SC.Content>
+				<div>map</div>
+			</SC.MainWrapper>
+		</Wrapper>
 	);
 };
 
