f93e8c0fe0e6f5d9804d365453d56ce3b1608c82
nadavmoreno <nadav@soficoop.com>
2020-12-01 12:08:44 +0200

    global layout fix

--- client/src/components/Wrapper/Wrapper.jsx
+++ client/src/components/Wrapper/Wrapper.jsx
@@ -3,21 +3,25 @@ import PropTypes from 'prop-types'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Navigation } from 'shared';
 import Footer from '../Footer';
+import * as SC from './style';
+
 
 const Wrapper = (props) => {
 	const { children } = props;
 
 	return (
 		<Fragment>
-			<Navigation />
+			<Navigation/>
 			{!children ? (
 				<div style={{ textAlign: 'center' }}>
-					<CircularProgress />
+					<CircularProgress/>
 				</div>
 			) : (
-				props.children
+				<SC.ChildrenWrapper>
+					{props.children}
+				</SC.ChildrenWrapper>
 			)}
-			<Footer />
+			<Footer/>
 		</Fragment>
 	);
 }
