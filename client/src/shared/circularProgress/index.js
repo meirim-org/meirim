58d4e1459ee06b7e8e9c8603bd164dee8410230b
Esraa Jbara <jbesraa@gmail.com>
2020-12-02 08:24:56 +0200

    add new circularprogress component

---
+++ client/src/shared/circularProgress/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import  MUICircularProgress from '@material-ui/core/CircularProgress';
+import styled from 'styled-components'
+
+const CircularWrapper = styled.div `
+	display: grid; 
+	justify-content: center; 
+	padding-top: 40%;
+`
+
+const CircularProgress = () => {
+	return (
+		<CircularWrapper> 
+			<MUICircularProgress /> 
+		</CircularWrapper>
+	)
+}
+
+export default CircularProgress
