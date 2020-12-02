9f23b64eb1cafc1b192950911364383cb81a3158
florpor <florist.powers@gmail.com>
2020-12-02 22:54:38 +0200

    port update main page title text to tmp-prod
    original commit a2c095f7df6590ce1f248ce787d4051d711b84a5

--- src/locale/he_IL.js
+++ src/locale/he_IL.js
@@ -25,8 +25,8 @@ export default {
     whatToRegister: "רוצים השכמה?",
     whyRegister: (
         <div>
-            רוצים לדעת אם הגינה הציבורית שלכם עומדת{" "}
-            <strong>להפוך למגדל?</strong>
+            רוצים לדעת מה בונים לכם{" "}
+            <strong>ליד הבית?</strong>
         </div>
     ),
     howItWorks: `
