a760c4bbf1a0f50ecf3835140a2f016da002e0d8
Esraa Jbara <jbesraa@gmail.com>
2020-12-06 19:54:33 +0200

    semi c fixes

--- client/src/shared/navigation/index.jsx
+++ client/src/shared/navigation/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { notAuthenticated } from 'redux/user/slice';
-import { logout } from 'services/user'
+import { logout } from 'services/user';
 import { Hidden } from '@material-ui/core';
-import { UserSelectors } from 'redux/selectors'
-import MobileNavBar from './mobile'
+import { UserSelectors } from 'redux/selectors';
+import MobileNavBar from './mobile';
 import DesktopNavBar from './desktop';
 import { useDispatch } from 'react-redux';
 
 const Navigation = () => {
-	const dispatch = useDispatch()
-	const { isAuthenticated, user } = UserSelectors()
-	
+	const dispatch = useDispatch();
+	const { isAuthenticated, user } = UserSelectors();
+
 	const logoutHandler = async () => {
-		const response = await logout()
-		if (response.status === 'OK') dispatch(notAuthenticated())
-	}
+		const response = await logout();
+		if (response.status === 'OK') dispatch(notAuthenticated());
+	};
 	
 	return (
 		<React.Fragment>
@@ -22,6 +22,6 @@ const Navigation = () => {
 			<Hidden mdDown> <DesktopNavBar logoutHandler={logoutHandler} user={user} isAuthenticated={isAuthenticated}/></Hidden>
 		</React.Fragment>
 	);
-}
+};
 
 export default Navigation;
