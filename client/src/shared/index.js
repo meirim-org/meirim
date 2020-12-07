c74d0768a83af84cafdc51d7afa5314d9fc375b3
Gal Gendler <ggendler@gmail.com>
2020-12-07 14:28:03 +0200

    New shared component - a styled checkbox

--- client/src/shared/index.js
+++ client/src/shared/index.js
@@ -3,8 +3,9 @@ import TextInput from './input';
 import Button from './button';
 import Dropdown from './dropdown';
 import TextArea from './textArea';
+import Checkbox from './checkbox';
 import Link from './link';
 
 export {
-	Button, Dropdown, TextArea, TextInput, Modal, Link,
+	Button, Dropdown, TextArea, TextInput, Modal, Link, Checkbox
 };
