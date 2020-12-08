67fcbf5a5703d88a963892cc27055a415aa3f7c6
Esraa Jbara <jbesraa@gmail.com>
2020-12-08 15:00:08 +0200

    pass props to modal children

--- client/src/shared/modal/index.jsx
+++ client/src/shared/modal/index.jsx
@@ -47,7 +47,7 @@ const modalComponents = {
 }
 
 const Modal = ({ id }) => {
-	const { open, modalType } = ModalSelectors()
+	const { open, modalType, modalProps } = ModalSelectors()
 	const ModalChildren = modalComponents[modalType]
 
 	return (
@@ -57,7 +57,7 @@ const Modal = ({ id }) => {
 					<IconWrapper>
 						<StyledIcon onClick={ModalActions().close} />
 					</IconWrapper>
-					{modalType && <ModalChildren/>}
+					{modalType && <ModalChildren {...modalProps}/>}
 				</ModalContentWRapper>
 			</StyledModal>
 		</ModalWrapper>
