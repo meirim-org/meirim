db33b00144c18f79f2e85e022174bc339d2e9363
Esraa Jbara <jbesraa@gmail.com>
2020-12-09 09:57:34 +0200

    add if for mobile/desktop in index

--- client/src/pages/Plan/index.jsx
+++ client/src/pages/Plan/index.jsx
@@ -1,35 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Wrapper from 'components/Wrapper';
-import { Header, GoalsPanel, StatusTypeUrlPanel, StatsPanel } from './components';
+import { withGetScreen } from 'react-getscreen';
+import PlanDesktop from './desktop';
+import PlanMobile from './mobile';
 import { useDataHandler } from './hooks';
-import * as SC from './style';
 
-const Plan = () => {
+const Plan = ({ isMobile }) => {
 	const [tabValue, setValue] = React.useState(0);
 	const handleTabChange = (_, newValue) => setValue(newValue);
 	const { id: planId } = useParams();
 	const { planData, dataArea, textArea } = useDataHandler(planId);
 	
-	return (
-	    <Wrapper>
-			<SC.MainWrapper>
-				<SC.Content>
-					<Header 
-						tablValue={tabValue} handleTabChange={handleTabChange} 
-						name={planData.name} countyName={planData.countyName} />
-					<SC.Main>
-						<StatusTypeUrlPanel 
-							tabValue={tabValue} type={planData.type} 
-							status={planData.status} url={planData.url} />
-						<GoalsPanel goalsFromMavat={planData.goalsFromMavat} tabValue={tabValue} />
-						<StatsPanel tabValue={tabValue} dataArea={dataArea} textArea={textArea} />
-					</SC.Main>
-				</SC.Content>
-				<div>map</div>
-			</SC.MainWrapper>
-		</Wrapper>
-	);
+	if (!isMobile) return <PlanDesktop tabValue={tabValue} handleTabChange={handleTabChange} 
+		planData={planData} dataArea={dataArea} textArea={textArea}/>;
+	else return <PlanMobile/>; 
 };
 
-export default Plan;
+export default withGetScreen(Plan);
---
+++ client/src/pages/Plan/mobile/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlanMobile = () => {
+	return (
+		<div>Plan page mobile version</div>
+	);
+};
+
+export default PlanMobile;
