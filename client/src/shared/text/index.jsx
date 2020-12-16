7fbde38d809e46c195ee1c99873041717acf41d1
Nadav Moreno <nadav@soficoop.com>
2020-12-17 00:44:07 +0200

    wip

--- client/src/shared/text/index.jsx
+++ client/src/shared/text/index.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Box } from '@material-ui/core';
 
-const StyledText = styled(Box)`
+const StyledText = styled.div`
     font-size: ${props => props.size};
     font-weight: ${props => props.weight};
     color: ${props => props.color};
     line-height: ${props => props.lineHeight};
 `;
 
-const Text = ({ component, size, weight, color, lineHeight }) => (
-	<StyledText 
-		as={component}
-		size={size} 
-		weight={weight} 
-		color={color}
-		lineHeight={lineHeight}
-	  />
-);
+const Text = ({ component, size, weight, color, lineHeight, text }) => {
+
+	return (
+		<StyledText 
+			as={component}
+			size={size} 
+			weight={weight} 
+			color={color}
+			lineHeight={lineHeight}
+		>
+			{text}
+		</StyledText>
+	);
+
+};
+
+
+
+Text.defaultProps = {
+	component: 'span',
+	size: '16px',
+	weight: '400',
+	lineHeight: '1.3',
+	color: '#000000'
+};
+
 
 Text.propTypes = {
-	component: PropTypes.string.isRequired,
+	component: PropTypes.string,
 	size: PropTypes.string,
 	weight: PropTypes.string,
 	color: PropTypes.string,
 	lineHeight: PropTypes.string,
+	text: PropTypes.string.isRequired,
 };
 
 export default Text;
