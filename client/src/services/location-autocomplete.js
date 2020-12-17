7e2b1cf14d091aeaabcacdb5c75ef98d188aed57
florpor <florist.powers@gmail.com>
2020-12-17 21:56:13 +0200

    allow location service initialization and use config for key

--- client/src/services/location-autocomplete.js
+++ client/src/services/location-autocomplete.js
@@ -1,5 +1,3 @@
-const {baseURL} = require('../config.json').locationServices;
-
 const loadScript = (url, callback) => {
     let script = document.createElement('script');
     script.type = 'text/javascript';
@@ -19,12 +17,12 @@ const loadScript = (url, callback) => {
     document.getElementsByTagName('head')[0].appendChild(script);
 };
 
-const getGoogleObj = () => {
+module.exports.init = () => {
     return new Promise((resolve, reject) => {
         if (window.google) {
             resolve(window.google);
         } else {
-            loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_KEY}&language=iw&libraries=places`, () => {
+            loadScript(`https://maps.googleapis.com/maps/api/js?language=iw&libraries=places&key=${process.env.CONFIG.mapsApiKey}`, () => {
                 if (window.google) {
                     resolve(window.google);
                 } else {
@@ -35,13 +33,15 @@ const getGoogleObj = () => {
     });
 };
 
-module.exports.autocomplete = function (text) {
-    return getGoogleObj().then((google) => {
-        const autocompleteService = new google.maps.places.AutocompleteService()
+module.exports.autocomplete = (input) => {
+    if (!window.google) {
+        return Promise.reject('location service not initialized');
+    } else {
+        const autocompleteService = new window.google.maps.places.AutocompleteService()
 
         return new Promise((resolve, reject) => {
-            autocompleteService.getPlacePredictions({input: text, componentRestrictions: {country: 'il'}}, (results, status) => {
-                if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            autocompleteService.getPlacePredictions({input, componentRestrictions: {country: 'il'}}, (results, status) => {
+                if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
                     reject(status);
                 } else {
                     resolve(results.map((r) => {
@@ -53,22 +53,24 @@ module.exports.autocomplete = function (text) {
                 }
             });
         });
-    });
+    }
 };
 
-module.exports.getPlaceData = function (placeId) {
-    return getGoogleObj().then((google) => {
-        const geocoder = new google.maps.Geocoder();
+module.exports.getPlaceLocation = (placeId) => {
+    if (!window.google) {
+        return Promise.reject('location service not initialized');
+    } else {
+        const geocoder = new window.google.maps.Geocoder();
 
         return new Promise((resolve, reject) => {
             geocoder.geocode({placeId}, (results, status) => {
-                if (status !== google.maps.GeocoderStatus.OK) {
+                if (status !== window.google.maps.GeocoderStatus.OK) {
                     reject(status);
                 } else {
                     resolve(results[0].geometry.location);
                 }
-            })
-        })
-    });
+            });
+        });
+    }
 };
 
