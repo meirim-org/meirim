ea4a55a7412d5109b74997bcc0d9326403cf667c
Esraa Jbara <jbesraa@gmail.com>
2020-12-20 02:34:29 +0200

    export new reducers

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,10 +1,14 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import modalReducer from './modal/slice';
 import userReducer from './user/slice';
+import commentsReducer from './comments/slice';
+import planReducer from './plan/slice';
 
 const rootReducer = combineReducers({
 	modal: modalReducer,
-	user: userReducer
+	user: userReducer,
+	comments: commentsReducer,
+	plan: planReducer
 });
 
 export default rootReducer;
