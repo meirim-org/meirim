f699bf90d4f2f315ea5d7b4bc227aed8e9a23d41
Esraa Jbara <jbesraa@gmail.com>
2020-12-20 02:34:29 +0200

    export new reducers

--- client/src/redux/selectors.js
+++ client/src/redux/selectors.js
@@ -1,7 +1,12 @@
 import ModalSelectors from './modal/selectors';
 import UserSelectors from './user/selector';
+import CommentSelectors from './comments/selectors';
+import PlanSelectors from './comments/selectors';
+
 
 export { 
+	PlanSelectors, 
+	CommentSelectors,
 	UserSelectors,
 	ModalSelectors
 };
