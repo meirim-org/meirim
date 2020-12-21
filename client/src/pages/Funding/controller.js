81ef06e270469901a1af0cba0a65c0d545bd4bf8
Gal Gendler <ggendler@gmail.com>
2020-12-21 14:24:20 +0200

    changes after PR

--- client/src/pages/Funding/controller.js
+++ client/src/pages/Funding/controller.js
@@ -9,13 +9,3 @@ export const createPaymentLink = async (options) => {
 		throw new Error(err);
 	}
 }
-
-export const registerUser = async (options) => {
-	//const requestData = { email };
-	try {
-		const response = await api.getPaymentURL({ amount:500 });
-		return response;
-	} catch (err) {
-		throw new Error(err);
-	}
-}
