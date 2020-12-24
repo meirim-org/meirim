483a68dec568310eb7aec9ef3311cfaf564a18d1
Nadav Moreno <nadav@soficoop.com>
2020-12-24 10:28:15 +0200

    migration file fixed

--- server/migrations/20203011215200_add_timestamps_to_comment.js
+++ server/migrations/20203011215200_add_timestamps_to_comment.js
@@ -1,6 +1,5 @@
 exports.up = function(knex) {
 	return knex.schema.table('comment', table => {
-		table.timestamps();
 		table.integer('likes');
     table.string('type');
 	});
@@ -9,8 +8,6 @@ exports.up = function(knex) {
 exports.down = function(knex) {
 	return knex.schema.table('comment', table => {
 		table.dropColumns(
-			'updated_at',
-			'created_at',
 			'likes',
 			'type'
 		);
