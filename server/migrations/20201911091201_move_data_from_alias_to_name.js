97351be24f1b21df3afe07308deea08ef3b3dbb0
florpor <florist.powers@gmail.com>
2020-12-28 00:03:03 +0200

    fix latest person properties migrations down functions

--- server/migrations/20201911091201_move_data_from_alias_to_name.js
+++ server/migrations/20201911091201_move_data_from_alias_to_name.js
@@ -1,10 +1,17 @@
 exports.up = function(knex) {
 	try { 
-	const updateQuery = `UPDATE person SET name = alias`
-	return knex.raw(updateQuery)
+		const updateQuery = `UPDATE person SET name = alias`;
+		return knex.raw(updateQuery);
 	} catch(err) {
-		console.log('err in migration', err)
+		console.log("err in migration", err);
 	}
 };
 
-exports.down = function() {};
+exports.down = function(knex) {
+	try {
+		const updateQuery = `UPDATE person SET alias = name`;
+		return knex.raw(updateQuery);
+	} catch(err) {
+		console.log("err in migration", err);
+	}
+};
