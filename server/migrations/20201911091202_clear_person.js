685244f6dd7f5454499be73da2f14ce043f88e50
florpor <florist.powers@gmail.com>
2020-12-28 00:03:03 +0200

    fix latest person properties migrations down functions

--- server/migrations/20201911091202_clear_person.js
+++ server/migrations/20201911091202_clear_person.js
@@ -1,7 +1,13 @@
 exports.up = function(knex) {
-	return knex.schema.table('person', table => {
+	return knex.schema.table("person", table => {
 		table.dropColumns("firstName", "lastName", "alias")
 	});
 };
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+	return knex.schema.table("person", table => {
+		table.string("firstName", 36).nullable();
+		table.string("lastName", 36).nullable();
+		table.string("alias").unique().nullable();
+	});
+};
