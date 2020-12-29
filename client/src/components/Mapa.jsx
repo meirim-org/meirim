8ea4996bdc19f8796107853c40525fdf53fb219e
florpor <florist.powers@gmail.com>
2020-12-29 14:44:43 +0200

    add map component geojson layer key so the layer is replaced when the geometry changes

--- client/src/components/Mapa.jsx
+++ client/src/components/Mapa.jsx
@@ -8,6 +8,10 @@ const Mapa = (props) =>  {
 	
 	if (!geom) return null;
 	const bounds = leaflet.geoJSON(geom).getBounds();
+
+	// hash the geom to create a key for the layer so react replaces the component properly
+	// since updated GeoJson layers are not updated after mount according to docs
+	const geomHash = JSON.stringify(geom).split('').reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
 	
 	return (
 		<Map
@@ -31,8 +35,8 @@ const Mapa = (props) =>  {
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			/>
-			{ 
-				geom && <GeoJSON data={geom} />
+			{
+				geom && <GeoJSON key={geomHash} data={geom} />
 			}
 			<div className="map-title">
 				{countyName && <button className="btn btn-light disabled">{countyName}</button>}
