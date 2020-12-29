75b2944364480410af64e1c402543bf600b8fcad
Nadav Moreno <nadav@soficoop.com>
2020-12-29 20:56:25 +0200

    css cleanup - wip

--- client/src/pages/Plan/mobile/style.js
+++ client/src/pages/Plan/mobile/style.js
@@ -54,18 +54,6 @@ export const CommentsWrapper = withTheme(styled.div`
     
 `);
 
-export const SubCommentForm = withTheme(styled.div`
-    grid-area: comments; 
-    border-top: 1px solid ${props => props.theme.palette.gray['300']};
-`);
-
-export const Header = withTheme(styled.div`
-    display: grid;
-    grid-template-columns: 49% 1fr;
-    padding: 1rem 4.8rem 0 1.2rem;
-    border-bottom: 1px solid ${props => props.theme.palette.gray['300']};
-`);
-
 export const Main = withTheme(styled.main`
     background-color:  ${props => props.theme.palette.gray['bg']};
     padding: 2rem 1rem;
@@ -74,72 +62,6 @@ export const Main = withTheme(styled.main`
         height: 100vh;
     }
 `);
-
-export const AppBar = withTheme(styled(MuiAppBar)`
-    background-color:  transparent !important;
-    color: black !important;
-    box-shadow: none !important;
-    .MuiTab-root {
-        min-width: auto !important;
-        padding: .4rem 1.5rem;
-    }
-    .MuiTabs-indicator {
-        background-color:  ${props => props.theme.palette.primary.main} !important;
-    }
-    .Mui-selected {
-        outline: 0 !important;
-        color:  ${props => props.theme.palette.primary.main} !important;
-    }
-    .MuiBadge-root {
-        align-items: center;
-    }
-    .MuiBadge-badge {
-        position: relative;
-        margin-right: .25rem;
-        transform: none;
-        color: ${props => props.theme.palette.primary['600']} !important;
-        background-color: ${props => props.theme.palette.primary['bg']} !important;
-    }
-`);
-
-export const TitlesAndTabs = styled.div`
-`;
-
-export const Buttons = withTheme(styled.div`
-    text-align: left;
-    margin: 0 -.25rem;
-    .MuiButton-containedPrimary {
-        background-color: transparent !important;
-        border: solid 1px #cdc9d8;
-        box-shadow: none;
-        color: ${props => props.theme.palette.black['100']} !important;
-    }
-    .MuiButton-startIcon {
-        margin: 0;
-    }
-    .MuiButton-root {
-        padding: .4rem .35rem;
-        margin: 0 .25rem;
-        &:hover, &:focus {
-            box-shadow: none;
-            outline: 0 !important;
-        }
-    }
-    .MuiButton-label > span {
-        padding: 0 .25rem;
-    }
-    .MuiButton-startIcon svg{
-        fill: ${props => props.theme.palette.secondary.contrastForGraphics} !important;
-    }
-`);
-
-export const SubTitleWrapper = styled.div`
-    margin-bottom: 1rem;
-`;
-
-export const TitleWrapper = styled.div`
-    margin-bottom: 3rem;
-`;
 
 export const NoComments = styled.div`
     text-align: center;
