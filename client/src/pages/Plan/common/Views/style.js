fb72ae989e08de2f19eb05f98e45c1386534c82c
Nadav Moreno <nadav@soficoop.com>
2020-12-29 20:42:52 +0200

    css cleanup - wip

--- client/src/pages/Plan/common/Views/style.js
+++ client/src/pages/Plan/common/Views/style.js
@@ -1,17 +1,7 @@
 import styled from 'styled-components';
 import { withTheme } from '@material-ui/core/styles';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import LinkIcon from '@material-ui/icons/Link';
-import CloseIcon from '@material-ui/icons/Close';
 import SubdirectoryArrowLeftRoundedIcon from '@material-ui/icons/SubdirectoryArrowLeftRounded';
-import { 
-	FormControl as MuiFormControl,
-	RadioGroup as MuiRadioGroup,
-	TextareaAutosize as MuiTextareaAutosize,
-
-} from '@material-ui/core';
-import { TabPanel } from 'shared';
-
 
 export const Header = styled.span`
     grid-area: header; 
@@ -57,54 +47,10 @@ export const Text = withTheme(styled.div`
 `);
 
 
-export const AddSubComment = withTheme(styled.div`
-    grid-area: add-comment;
-    padding: 1rem; 
-    text-align: center;
-    .MuiSvgIcon-root {
-        font-size: 1.125rem !important;
-        fill: ${props => props.theme.palette.primary['600']} !important;  
-        margin: 0 0.75rem; 
-    }
-    
-    .MuiButton-label {
-        font-weight: 300;
-        font-size: 14px;
-        padding: .4rem 0 .4rem .75rem;
-        border-radius: 200px;
-    }
-    
-   &.active .MuiButton-label{
-        background-color: ${props => props.theme.palette.gray['bg']} !important; 
-    }
-    
-`);
-
-
 export const CommentIcon = styled(ChatBubbleOutlineIcon)`
     font-size: 1rem !important;
 `;
 
-
-export const FormControl = withTheme(styled(MuiFormControl)`
-    margin-bottom: 1rem !important;
-    textarea {
-        width: 100%;
-        padding: 1rem;
-        border-radius: 4px;
-        border-color: ${props => props.theme.palette.gray['600']} !important;  
-        
-        &:focus {
-            outline-color: ${props => props.theme.palette.primary['600']} !important;
-        }
-    }   
-`);
-
-export const addSubCommentButtonWrapper = styled.div`
-    margin: 0 -.6rem;
-    display: flex;
-    justify-content: flex-end;
-`;
 
 export const addSubCommentWrapper = styled.div`
     padding: 2rem 3.5rem;
@@ -123,62 +69,6 @@ export const addCommentButtonWrapper = styled.div`
         margin-bottom: 0;
     }
 `;
-
-export const ErrorWrapper = styled.div`
-    position: absolute;
-    left: 0;
-    top: 50%;
-    transform: translate(-100%, calc(-50% - .5rem));
-    padding: 0 1rem;
-`;
-
-
-export const NewCommentControl = withTheme(styled(MuiFormControl)`
-    .MuiTypography-root {
-        font-family:  ${props => props.theme.fontFamily} !important;
-        font-size: 0.875rem;
-        color: ${props => props.theme.palette.black} !important;
-    }
-    .MuiRadio-colorSecondary {
-        color: ${props => props.theme.palette.gray['main']} !important;
-        &.Mui-checked {
-            color: ${props => props.theme.palette.primary['700']} !important;
-        }
-    }
-    
-    textarea {
-        border-color: ${props => props.theme.palette.primary.main} !important;
-        border-radius: 12px;
-        padding: 1rem;
-        margin-bottom: 1rem;
-    }   
-`);
-
-export const NewCommentTabPanel = withTheme(styled(TabPanel)`
-    &.no-comments {
-        margin-bottom: 5.8rem;
-    }
-`);
-
-export const RadioGroup = withTheme(styled(MuiRadioGroup)`
-    margin: 0 -.5rem;
-`);
-
-export const NewCommentLabelWrapper = withTheme(styled.div`
-    padding: 0 .5rem;
-    
-    .MuiButtonBase-root {
-        background-color: transparent !important;
-        padding: 0.187rem 0.375rem;
-    }
-`);
-
-export const TextareaAutosize = withTheme(styled(MuiTextareaAutosize)`
-    &[disabled] {
-        border-color: ${props => props.theme.palette.gray['300']} !important;
-        background-color: ${props => props.theme.palette.white} !important;
-    }
-`);
 
 export const SubCommentBox = withTheme(styled.div`
     background-color: ${props => props.theme.palette.white} !important;  
@@ -204,47 +94,12 @@ export const ArrowIcon = withTheme(styled(SubdirectoryArrowLeftRoundedIcon)`
 `);
 
 
-export const PlanSummaryTitleWrapper = styled.div`
-    margin-bottom: .75rem;
-`;
-
-export const PlanTermsWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    margin: 0 -.25rem .75rem;
-`;
-
-export const PlanTermWrapper = withTheme(styled.div`
-    padding: .25rem;
-    > .MuiChip-root {
-        height: auto;
-        min-height: 1.875rem;
-        color: ${props => props.theme.palette.green['text']} !important;  
-        background-color: ${props => props.theme.palette.green['bg']} !important;  
-    }
-`);
-
 export const StatusAndTypeWrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     margin: 0 -.5rem 1rem;
 `;
 
-export const StatusWrapper = styled.div`
-    padding: 0 0.5rem
-`;
-
-export const TypeWrapper = styled.div`
-    padding: 0 0.5rem
-`;
-
-export const UrlWrapper = styled.div``;
-
-export const CustomLinkIcon = withTheme(styled(LinkIcon)`
-    fill: ${props => props.theme.palette.secondary.main} !important;  
-    vertical-align: middle;
-    margin-right: .5rem;
-`);
 
 export const EntryContent = withTheme(styled.div`
     font-family:  ${props => props.theme.fontFamily} !important;
@@ -252,16 +107,6 @@ export const EntryContent = withTheme(styled.div`
     line-height: 1.5;
     color: ${props => props.theme.palette.black} !important;  
 `);
-
-export const SemiBold = styled.span`
-    font-weight: 600;
-`;
-
-
-export const ChartWrapper = styled.div`
-    height: 200px;
-    margin-top: 1rem;
-`;
 
 export const SubscribeIconWrapper = withTheme(styled.div`
     background-color: ${props => props.theme.palette.white};
@@ -282,27 +127,4 @@ export const SubscribeTextWrapper = styled.div`
     text-align: center;
     margin-bottom: 1.25rem;
 `;
-
-export const SubscribeButtonsWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    margin: 0 -.75rem;
-`;
-
-export const SubscribeButtonWrapper = styled.div`
-    padding: 0 .75rem;
-`;
-
-export const CloseSubscribeIcon = withTheme(styled(CloseIcon)`
-    position: absolute;
-    top: .5rem;
-    right: .5rem;
-    cursor: pointer;
-    font-size: 1.3rem !important;
-    fill: ${props => props.theme.palette.black} !important;  
-    opacity: .8;
-`);
-
 
