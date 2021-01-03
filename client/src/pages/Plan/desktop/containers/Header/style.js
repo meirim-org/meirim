4510837288aa6c6255ed0b7188ea89535b95df07
Nadav Moreno <nadav@soficoop.com>
2021-01-03 16:23:49 +0200

    tabs styling

--- client/src/pages/Plan/desktop/containers/Header/style.js
+++ client/src/pages/Plan/desktop/containers/Header/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { AppBar as MuiAppBar } from '@material-ui/core';
+import { AppBar as MuiAppBar, Button } from '@material-ui/core';
 import { withTheme } from '@material-ui/core/styles';
 
 export const SubTitleWrapper = styled.div`
@@ -85,5 +85,26 @@ export const AppBar = withTheme(styled(MuiAppBar)`
     }
     .MuiTab-wrapper {
         font-size: 1rem !important;
+    }
+`);
+
+
+export const Tab = withTheme(styled(Button)`
+    border-radius: 0 !important;
+    border-bottom: 2px solid transparent !important;
+    outline: 0 !important;
+    padding-right: 1.5rem !important;
+    padding-left: 1.5rem !important;
+    
+    .MuiButton-label {
+        color:  ${props => props.theme.palette.black} !important;
+        font-size:  16px !important;
+    }
+    
+    &.active {
+        border-color: ${props => props.theme.palette.primary.main} !important;
+        .MuiButton-label {
+            color:  ${props => props.theme.palette.primary.main} !important;
+        }
     }
 `);
