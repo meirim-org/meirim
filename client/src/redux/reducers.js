6a0ec3c0251d7b780eddce6208ca18b9c2f6753e
florpor <florist.powers@gmail.com>
2021-01-09 17:12:36 +0200

    funding page - add funding stats

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -3,12 +3,14 @@ import modalReducer from './modal/slice';
 import userReducer from './user/slice';
 import commentsReducer from './comments/slice';
 import planReducer from './plan/slice';
+import fundingReducer from './funding/slice';
 
 const rootReducer = combineReducers({
 	modal: modalReducer,
 	user: userReducer,
 	comments: commentsReducer,
-	plan: planReducer
+	plan: planReducer,
+	funding: fundingReducer
 });
 
 export default rootReducer;
