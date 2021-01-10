a8dcc04a2e7386260f0b40c85a8b6b1ef52a5bd4
Esraa Jbara <jbesraa@gmail.com>
2021-01-10 18:41:07 +0200

    add client-server hookup

--- client/src/pages/Plan/desktop/containers/Header/components/savePlan/index.jsx
+++ client/src/pages/Plan/desktop/containers/Header/components/savePlan/index.jsx
@@ -3,13 +3,16 @@ import { useTheme } from '@material-ui/styles';
 import { Button } from '@material-ui/core';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { Text } from 'shared';
+import { subscribeUserToPlan } from 'pages/Plan/controller';
 import t from 'locale/he_IL';
+import { useParams } from 'react-router-dom';
 
 const SavePlan = () => {
+	const { id } = useParams();
 	const theme = useTheme();
 	
 	return (				
-		<Button variant="contained" color="primary" startIcon={<StarBorderIcon />}>
+		<Button onClick={() => subscribeUserToPlan(id)} variant="contained" color="primary" startIcon={<StarBorderIcon />}>
 			<Text size="14px" text={t.saving} component="span" color={theme.palette.gray['800']}/>
 		</Button>
 	);
