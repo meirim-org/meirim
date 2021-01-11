deb912da8d3e8cf69d20c60297dabfc269eb4c3c
naortor <naortor@gmail.com>
2021-01-11 20:02:13 +0200

    wip

--- client/src/pages/Homepage/CommunityDiscussions.jsx
+++ client/src/pages/Homepage/CommunityDiscussions.jsx
@@ -3,27 +3,19 @@ import styled from 'styled-components';
 import { H2, CommonSection } from './style';
 import Preview from './Preview';
 import communityDiscussionsImage from '../../assets/community-discussions.svg';
-import { device } from 'style';
 
 const CommunityDiscussionsSection = styled(CommonSection)`
 	margin-top: 60px;
 `; 
-const CommunityDiscussionsImage = styled.img.attrs(()=>({ src: communityDiscussionsImage }))`
-	@media ${device.tablet} {
-		max-width: 100%; 
-	}
-`;
 
 const CommunityDiscussions = () => {
 
 	return (
 		<CommunityDiscussionsSection>
 			<H2>דיונים בקהילה</H2>
-			<Preview>
-				<CommunityDiscussionsImage />
-			</Preview>
+			<Preview src={communityDiscussionsImage} />
 		</CommunityDiscussionsSection>
-	)
-}
+	);
+};
 
 export default CommunityDiscussions;
--- client/src/pages/Homepage/TopViews.jsx
+++ client/src/pages/Homepage/TopViews.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { H2, CommonSection } from './style';
 import Preview from './Preview';
 import topPlansImage from '../../assets/top-plans.svg';
-import { device } from 'style';
 
 const TopViewsSection = styled(CommonSection)``; 
-const TopPlansImage = styled.img.attrs(()=>({ src: topPlansImage }))` 
-	@media ${device.tablet} {
-		max-width: 100%;
-	}
-`;
 
 const TopViews = () => {
 
 	return (
 		<TopViewsSection>
 			<H2>התוכניות הכי נצפות השבוע</H2>
-			<Preview>
-				<TopPlansImage />
+			<Preview src={topPlansImage}>
 			</Preview>
 		</TopViewsSection>
-	)
-}
+	);
+};
+
+TopViews.propTypes = {
+	isMobile: PropTypes.func.isRequired,
+};
 
 export default TopViews;
