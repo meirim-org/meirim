810b13d65160901967ae70acb724d8f3b6d65239
nadavmoreno <nadavmoreno@gmail.com>
2021-01-12 08:23:03 +0200

    wip

--- client/src/pages/UserPlans/style.js
+++ client/src/pages/UserPlans/style.js
@@ -4,6 +4,7 @@ import { withTheme } from '@material-ui/core/styles';
 
 export const TitleWrapper = withTheme(styled.div`
     margin-bottom: 1rem;
+    padding-top: 2rem;
 `);
 
 export const NoPlansContent = withTheme(styled.div`
@@ -12,5 +13,23 @@ export const NoPlansContent = withTheme(styled.div`
     > * {
         text-align: center;
     }
+    min-height: calc(100vh -  ${props => props.theme.navigation.desktop});
+    
+    svg {
+        width: 54px;
+        height: auto;
+        path {
+            &:first-child {
+                fill: ${props => props.theme.palette.secondary['100']} !important;
+            }
+            &:last-child {
+                fill: ${props => props.theme.palette.secondary.main} !important;
+                stroke: ${props => props.theme.palette.secondary.main} !important;
+            }
+        }           
+    }
+`);
+
+export const PlansContent = withTheme(styled.div`
     min-height: calc(100vh -  ${props => props.theme.navigation.desktop});
 `);
