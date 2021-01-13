3f325ac64b8a30b4c9597c10d7ad8df978a492c4
florpor <florist.powers@gmail.com>
2021-01-13 21:30:53 +0200

    update person to alias migration to create names for existing users who do not have aliases

--- server/migrations/20201119191201_move_data_from_alias_to_name.js
+++ server/migrations/20201119191201_move_data_from_alias_to_name.js
@@ -1,7 +1,17 @@
 exports.up = function(knex) {
-	try { 
-		const updateQuery = `UPDATE person SET name = alias`;
-		return knex.raw(updateQuery);
+	try {
+		// for users who set their alias - just copy that into the name column. for users
+		// who haven't set up a const name using their slightly-obfuscated id
+		return knex.raw(`UPDATE person SET name = alias`)
+			.then(() => knex.raw(
+				`UPDATE
+					person,
+					(
+						SELECT id, CONCAT('משתמש מהאתר הישן ', LPAD(ROUND(id + pc.c/5.7), 5, '0')) AS n
+						FROM person, (SELECT COUNT(*) as c FROM person) AS pc
+					) AS person_fake_names
+				SET name = person_fake_names.n
+				WHERE person.id = person_fake_names.id and person.alias IS NULL`));
 	} catch(err) {
 		console.log("err in migration", err);
 	}
@@ -9,7 +19,7 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
 	try {
-		const updateQuery = `UPDATE person SET alias = name`;
+		const updateQuery = `UPDATE person SET alias = name WHERE name NOT LIKE 'משתמש מהאתר הישן %'`;
 		return knex.raw(updateQuery);
 	} catch(err) {
 		console.log("err in migration", err);
