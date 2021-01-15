e857f7f068f1ff528eea007fd06498c03882f521
nadavmoreno <nadavmoreno@gmail.com>
2021-01-15 09:43:47 +0200

    style fix

---
+++ client/src/pages/Plan/desktop/containers/Header/components/savePlan/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Button as MuiButton } from '@material-ui/core';
+import { withTheme } from '@material-ui/core/styles';
+
+
+export const Button = withTheme(styled(MuiButton)`
+    &.MuiButton-containedPrimary {
+        transition: none !important;
+        &.active {
+        border-color: ${props => props.theme.palette.secondary.main} !important;
+        background-color: ${props => props.theme.palette.secondary['active']} !important;
+        }    
+    }
+`);
+
