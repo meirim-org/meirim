0e559c9af722d81dd97c0fcb999e008a48d973b6
gruppin <gruppin@gmail.com>
2021-01-16 16:25:16 +0200

    reason and appeal panels

--- client/src/pages/Tree/common/Panels/index.js
+++ client/src/pages/Tree/common/Panels/index.js
@@ -2,10 +2,12 @@ import TreeDetailsPanel from './details';
 import TreeReasonPanel from './reason'; 
 import SubscribePanel from './subscribe';
 import MapPanel from './map';
+import TreeAppealPanel from './appeal';
 
 export { 
 	TreeDetailsPanel,
 	SubscribePanel,
 	MapPanel,
-	TreeReasonPanel
+	TreeReasonPanel,
+	TreeAppealPanel
 };
