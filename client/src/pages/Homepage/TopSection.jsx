34f55e262fb3f4d9be17143360bf71d651454bb8
naortor <naortor@gmail.com>
2021-01-16 17:30:04 +0200

    Style fix

--- client/src/pages/Homepage/TopSection.jsx
+++ client/src/pages/Homepage/TopSection.jsx
@@ -2,21 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 import SearchBox from './SearchBox';
 import homepageImage from '../../assets/homepage.svg';
+import homepageMobileImage from '../../assets/homepage-mobile.svg'
 import Typography from 'shared/typography';
 import { device } from 'style';
 
 const Image = styled.div`
-    height: 223px;
+    height: 288px;
     width: 100%;
-    background-image: url(${homepageImage});
+    background-image: url(${homepageMobileImage});
+    background-color: #FCF9FF;
     background-repeat: no-repeat;
-    background-size: cover;
+    background-size: contain;
 
     @media ${device.tablet} {
+        background-image: url(${homepageImage});
+        background-size: auto;
         position: absolute;
         left: 0;
         right: 0;
-        height: 564px;
+        height: 588px;
+    }
+`;
+
+const Cover = styled.div`
+    @media ${device.tablet} {
+        position: absolute;
+        left:0; top: 0; right: 0; bottom: 0;
+        padding-top: 132px;
+        padding-right: 80px;
     }
 `;
 
@@ -26,22 +39,8 @@ const Section = styled.section`
     position: relative;
 
     @media ${device.tablet} {
-        height: 564px;
-    }
-`;
-
-const Cover = styled.div`
-    @media ${device.tablet} {
-        &:after, &:before {
-            content: none;
-        }
-        position: absolute;
-        left: 0;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        background: linear-gradient(270deg, #FFFFFF 34.63%, rgba(196, 196, 196, 0) 63.08%);
-        padding: 60px 80px 64px 0;
+        height: 588px;
+        background: #FCF9FF;
     }
 `;
 
@@ -49,16 +48,21 @@ const H1 = styled.h1`
     width: 100%;
     text-align:right;
     color: #270E78;
-    margin-top: 24px;
-    margin-bottom: 0px;
     font-size: 36px;
     line-height: 54px;
     padding-right: 16px;
+    z-index: 1;
+    margin-top: 20px;
+    margin-bottom: 24px;
 
     @media ${device.tablet} {
         font-size: 48px;
         line-height: 60px;
         padding-right: 0;
+        margin-top: 0;
+        margin-bottom: 16px;
+        background-color: #FCF9FF;
+        display: inline;
     }
 `;
 
@@ -70,13 +74,15 @@ const Paragraph = styled(Typography)`
     max-width: 328px;
     margin: 24px 0 0;
     padding-right: 16px;
+    z-index: 1;
 
     @media ${device.tablet} {
         margin: 0;
         margin-top: 24px;
         max-width: 495px;
-        font-size: 18px;
+        font-size: 20px;
         padding-right: 0;
+        background-color: #FCF9FF;
     }
 
 `;
@@ -85,13 +91,13 @@ const TopSection = () => {
 	return (
 		<Section>
 			<Image/>
-			<Cover>
+            <Cover>
                 <H1>ברוכים הבאים למעירים!</H1>
                 <Paragraph as='p'variant="paragraphText" mobileVariant="paragraphText">
                 קהילת מעירים מאפשרת לכם לקבל מידע תכנוני על הנעשה בסביבה הקרובה שלכם, ללמוד על תוכניות ונושאים שמעניינים אתכם ולהיות פעילים, יחד עם רבים אחרים, בתהליכי התכנון והבנייה בארץ.
                 </Paragraph>
                 <SearchBox />
-			</Cover>
+            </Cover>
 		</Section>
 	)
 }
