d16d9e89407afdc1b7d6b0fc228bf7aba5880942
Gal Gendler <ggendler@gmail.com>
2021-01-20 22:37:27 +0200

    micro copy + team members

--- client/src/shared/teamMembers/style.js
+++ client/src/shared/teamMembers/style.js
@@ -1,22 +1,33 @@
 import styled from 'styled-components';
+import { device } from '../../style';
 
 export const Member = styled.div`
+text-align:center;
+width:'100px';
+height:'150px';
+font-size:15px;
+img{
+    height:90px;
+    width:90px;
+}
+.name{
+    color:#391695;
+    text-align: center;
+}
+.title{
+    color:black;
+    text-align: center;
+}
+
+@media ${device.tablet} {
     width:'400px';
     height:'400px';
-    text-align:center;
     padding:15px;
     font-size:20px;
-    .name{
-        color:#391695;
-        text-align: center;
-    }
-    .title{
-        color:black;
-        text-align: center;
-    }
     img{
         height:126px;
         width:126px;
 
     }
-    `
+}
+`
