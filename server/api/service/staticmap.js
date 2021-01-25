9c7e79433bc04a9a0497a22040ba03b3a5b328b3
florpor <florist.powers@gmail.com>
2021-01-26 00:58:35 +0200

    move map marker image used by email staticmap to server

--- server/api/service/staticmap.js
+++ server/api/service/staticmap.js
@@ -8,7 +8,7 @@ const fetchStaticMap = (lat, lon) => {
 	const map = new StaticMaps(Config.get('staticmap'));
 
 	const marker = {
-		img: path.resolve('../client/public/images/map_marker.png'),
+		img: path.resolve(path.join(__dirname, 'email', 'map_marker.png')),
 		width: 32,
 		height: 32,
 		coord: [lon, lat]
