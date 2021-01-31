221404a96f420bdd54874a01a96e31895b86576c
Udi Oron <udioron@gmail.com>
2021-01-31 11:09:22 +0200

    cleanup eslint warnings: convert icons from jsx to svg.

--- client/src/assets/funding/index.js
+++ client/src/assets/funding/index.js
@@ -1,5 +1,5 @@
-export {default as map} from '../funding/mapIcon'
-export {default as notification} from '../funding/notoficationIcon'
-export {default as tree} from '../funding/treeIcon'
-export {default as community} from '../funding/communityIcon'
-export {default as construction} from '../funding/constructionIcon'
+export { ReactComponent as MapIcon } from './map-icon.svg';
+export { ReactComponent as NotificationIcon } from './notofication-icon.svg';
+export { ReactComponent as TreeIcon } from './tree-icon.svg';
+export { ReactComponent as CommunityIcon } from './community-icon.svg';
+export { ReactComponent as ConstructionIcon } from './construction-icon.svg';
