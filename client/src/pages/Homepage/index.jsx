169d20b37b4f912635c5cc17195f54bbb34011de
Udi Oron <udioron@gmail.com>
2021-02-08 13:04:51 +0200

    added useTitle

--- client/src/pages/Homepage/index.jsx
+++ client/src/pages/Homepage/index.jsx
@@ -4,16 +4,20 @@ import TopSection from './TopSection';
 import TopViews from './TopViews';
 import CommunityDiscussions from './CommunityDiscussions';
 import Wrapper from 'components/Wrapper';
+import t from '../../locale/he_IL';
+import { useTitle } from '../../hooks';
 
 const Homepage = () => {
-	return (
-		<Wrapper>
-			<TopSection />
-			<TopViews />
-			<WeNeedYou />
-			<CommunityDiscussions />
-		</Wrapper>
-	)
-}
+    useTitle(t.homepage);
+
+    return (
+        <Wrapper>
+            <TopSection />
+            <TopViews />
+            <WeNeedYou />
+            <CommunityDiscussions />
+        </Wrapper>
+    );
+};
 
 export default Homepage;
--- client/src/scenes/Alerts.jsx
+++ client/src/scenes/Alerts.jsx
@@ -2,78 +2,86 @@ import React, { useState } from 'react';
 import Wrapper from '../components/Wrapper';
 import Snackbar from '@material-ui/core/Snackbar';
 
-import { CheckIfUserCanAccessPage } from 'hooks';
+import { CheckIfUserCanAccessPage, useTitle } from 'hooks';
 import api from 'services/api';
 import './Alerts.css';
 import AlertPlans from '../components/AlertPlans';
 import AlertTrees from '../components/AlertTrees';
 import AlertList from '../components/AlertList';
+import t from '../locale/he_IL';
 
 const messages = {
-	alertAdded:'התראת תכנון התווספה בהצלחה!',
-	alertDeleted:'התראת תכנון הוסרה בהצלחה'
-}
+    alertAdded: 'התראת תכנון התווספה בהצלחה!',
+    alertDeleted: 'התראת תכנון הוסרה בהצלחה',
+};
 
 const Alerts = () => {
-	CheckIfUserCanAccessPage()
+    CheckIfUserCanAccessPage();
 
-	const [ added, setAdded ] = useState(false);
-	const [ deleted, setDeleted ] = useState(false);
-	const [ alerts, setAlerts ] = useState([]);
+    const [added, setAdded] = useState(false);
+    const [deleted, setDeleted] = useState(false);
+    const [alerts, setAlerts] = useState([]);
 
-	const handleCloseSnackbar = () => {
-		setAdded(false);
-		setDeleted(false);
-	};
+    const handleCloseSnackbar = () => {
+        setAdded(false);
+        setDeleted(false);
+    };
 
-	const handleAddedAlert = () => {
-		getAlerts();
-		setAdded(true);
-	};
+    const handleAddedAlert = () => {
+        getAlerts();
+        setAdded(true);
+    };
 
-	const handleDeletedAlert = () => {
-		getAlerts();
-		setDeleted(true);
-	};
+    const handleDeletedAlert = () => {
+        getAlerts();
+        setDeleted(true);
+    };
 
-	const getAlerts = () => {
-		return api.get('/alert')
-			.then(result => {
-				setAlerts(result.data);
-			})
-			.catch(error => console.error(error));
-	}
+    const getAlerts = () => {
+        return api
+            .get('/alert')
+            .then((result) => {
+                setAlerts(result.data);
+            })
+            .catch((error) => console.error(error));
+    };
 
-	React.useEffect(() => {
-		getAlerts();
-	}, []);
+    React.useEffect(() => {
+        getAlerts();
+    }, []);
 
-	return (
-		<Wrapper>
-			<div className="container alerts-container widedialog">
-				<AlertPlans notifyAddedAlert={handleAddedAlert} />
-				<AlertTrees notifyAddedAlert={handleAddedAlert} />
-				<AlertList notifyDeletedAlert={handleDeletedAlert} alerts={alerts} />
+    useTitle(t.alerts);
 
-			</div>
-			<Snackbar
-				open={added || deleted}
-				anchorOrigin={{
-					vertical: 'bottom',
-					horizontal: 'left'
-				}}
-				autoHideDuration={3000}
-				onClose={handleCloseSnackbar}
-				onClick={handleCloseSnackbar}
-				ContentProps={{
-					'aria-describedby': 'message-id'
-				}}
-				message={
-					<span id="message-id">{added ? messages.alertAdded : messages.alertDeleted}</span>
-				}
-			/>
-		</Wrapper>
-	);
-}
+    return (
+        <Wrapper>
+            <div className="container alerts-container widedialog">
+                <AlertPlans notifyAddedAlert={handleAddedAlert} />
+                <AlertTrees notifyAddedAlert={handleAddedAlert} />
+                <AlertList
+                    notifyDeletedAlert={handleDeletedAlert}
+                    alerts={alerts}
+                />
+            </div>
+            <Snackbar
+                open={added || deleted}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                autoHideDuration={3000}
+                onClose={handleCloseSnackbar}
+                onClick={handleCloseSnackbar}
+                ContentProps={{
+                    'aria-describedby': 'message-id',
+                }}
+                message={
+                    <span id="message-id">
+                        {added ? messages.alertAdded : messages.alertDeleted}
+                    </span>
+                }
+            />
+        </Wrapper>
+    );
+};
 
 export default Alerts;
