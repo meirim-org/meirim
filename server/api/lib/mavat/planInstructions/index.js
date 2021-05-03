041a0a4c6045def14cfe5450013b1aa791bf7585
TomerMe <tomer.laor1@gmail.com>
2021-05-03 23:20:13 +0300

    remove drawings from plans that got them

--- server/api/lib/mavat/planInstructions/index.js
+++ server/api/lib/mavat/planInstructions/index.js
@@ -1,3 +1,6 @@
+const HummusRecipe = require('hummus-recipe');
+const hummus = require('hummus');
+
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
@@ -15,8 +18,42 @@ const processPlanInstructionsFile = async (fileDir) => {
 	const pdfs = files.filter(fileName => fileName.endsWith('.pdf'));
 
 	if (pdfs.length > 0) {
-		// we just want the one file, at least for now
-		const filePath = path.join(fileDir, pdfs[0]);
+		const pdfFileName = pdfs[0];
+		let filePath = path.join(fileDir, pdfFileName);
+		let fileSizeInMB = fs.statSync(filePath).size / (1024 * 1024);
+
+		let numberOfIter = 0;
+
+		// the pdf extension that we are using to read table 3.2 is very problematic in memory usage.
+		// we know that the end of plans can contain heavy drawings.
+		// if the file is larger than 3 MB, the plan contains these drawings and we remove each page at a time
+		// until we got a decent file size.
+		while (fileSizeInMB > 3) {
+
+			const instream  = new hummus.PDFRStreamForFile(filePath);
+			const pdfReader = hummus.createReader(instream);
+			const pageCount = pdfReader.getPagesCount();
+			instream.close();
+
+			const pagesToAdd = [];
+			// everything but the last page
+			for (let i = 1; i < pageCount; i++) {
+				pagesToAdd.push(i);
+			}
+
+			const pathOfNewPdf = path.join(fileDir, `anotherTemp${numberOfIter}.pdf`);
+			const pdfDoc = new HummusRecipe('new', pathOfNewPdf);
+			pdfDoc
+				.appendPage(filePath, pagesToAdd)
+				.endPDF();
+
+			filePath = pathOfNewPdf;
+			fileSizeInMB = fs.statSync(filePath).size / (1024 * 1024);
+
+			numberOfIter++;
+
+		}
+
 		const data = await extractPdfData(filePath);
 		return data;
 	}
