756000e73c47c27929e82f38b1f65c776bac0fe0
Michal Mart <michal.mart@gmail.com>
2021-07-12 23:23:15 -0400

    Add planTag model methods & fix export

--- server/api/model/plan_tag.js
+++ server/api/model/plan_tag.js
@@ -25,5 +25,39 @@ class PlanTag extends Model {
 	getCollection () {
 		return this.collection();
 	}
+
+	static byPlan (planId) {
+		if (!planId) {
+			throw new Exception.BadRequest('Must provide planId');
+		}
+		return Knex('plan_tag')
+			.join('tag', 'tag.id', 'plan_tag.tag_id')
+			.select('tag.name','tag.display_name')
+			.where({ plan_id: planId })
+	}
+
+	static deletePlanTags ( planId) {
+		return this.query('where', 'plan_id', '=', planId)
+			.destroy()
+			.then(() => true);
+	}
+
+	static createPlanTags ( planId) {
+		// TODO recreate plan tags
+	}
+	
+	static async createPlanTag({ tagId, planId, displayScore, createdByDataRules }) {
+		await Bookshelf.transaction(async (transaction) => {
+				const data = {
+					tag_id: tagId,
+					plan_id: planId,
+					display_score: displayScore,
+					created_by_data_rules: createdByDataRules
+
+				};
+
+				await new PlanTag(data).save(null, {transacting: transaction});
+		});
+	}	
 }
-module.exports = Tag;
+module.exports = PlanTag;
