4e30ec49fb56e0368fc9ef0488a441eae4945ffe
ofir.katz <ofir.katz@plataine.com>
2021-07-27 20:32:16 +0300

    - lifted map title outside Mapa component. - added plan status box with status dot and text.

--- client/src/shared/planCard/index.jsx
+++ client/src/shared/planCard/index.jsx
@@ -7,6 +7,7 @@ import { Grid } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
 import UnsafeRender from 'components/UnsafeRender';
 import { Text } from 'shared';
+import styled from 'styled-components';
 
 const PlanCard = ({ plan }) => {
 	const theme = useTheme();
@@ -15,26 +16,37 @@ const PlanCard = ({ plan }) => {
 		<Grid item xs={12} sm={6} md={4}>
 			<SC.Card raised={true}>
 				<Link
-					className="card-link"
+					className='card-link'
 					to={`/plan/${plan.id}`}
 				>
 					<SC.CardActionArea>
 						<SC.CardMedia title={plan.PL_NUMBER}>
+							<MapTitle>
+								<StatusBox>
+									<StatusDot />
+									<Text 
+										size='14px'
+										weight='600'
+										text={'אישור עקרוני ב-5.4.21'}
+										color={theme.palette.black}
+									/>
+								</StatusBox>
+							</MapTitle>
 							<Mapa
 								geom={plan.geom}
 								countyName={plan.PLAN_COUNTY_NAME}
 								hideZoom={true}
 								disableInteractions={true}
-								title2={plan.distance?` ${Math.ceil(plan.distance/5)*5} מ׳ מהכתובת`:'' }
+								title2={plan.distance ? ` ${Math.ceil(plan.distance / 5) * 5} מ׳ מהכתובת` : ''}
 							/>
 						</SC.CardMedia>
 						<SC.CardContent>
 							<Text
-								size="1.5rem"
-								weight="600"
+								size='1.5rem'
+								weight='600'
 								text={plan.plan_display_name}
 								color={theme.palette.black}
-								component="h2"
+								component='h2'
 							/>
 							<UnsafeRender
 								html={
@@ -48,8 +60,6 @@ const PlanCard = ({ plan }) => {
 		</Grid>
 	);
 };
-	
-
 
 
 PlanCard.propTypes = {
@@ -57,3 +67,30 @@ PlanCard.propTypes = {
 };
 
 export default PlanCard;
+
+const MapTitle = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    z-index: 9999;
+    font-size: large;
+    padding: 5px;
+`;
+
+const StatusBox = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 1px 4px;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 4px;
+`;
+
+const StatusDot = styled.div`
+    width: 14px;
+    height: 14px;
+    margin-left: 7px;
+    border-radius: 7px;
+    background: ${({ approved }) => approved ? '#1976D2' : '#AE7FF0'};
+`;
