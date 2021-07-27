c68827a30d79caaa093d1d303453ae00663d48b1
ofir.katz <ofir.katz@plataine.com>
2021-07-27 20:36:53 +0300

    - changed card height according to media query.

--- client/src/shared/planCard/style.js
+++ client/src/shared/planCard/style.js
@@ -1,38 +1,50 @@
 import styled from 'styled-components';
 import { withTheme } from '@material-ui/core/styles';
 import {
-	Card as MuiCard,
-	CardMedia as MuiCardMedia,
-	CardContent as MuiCardContent,
-	CardActionArea as MuiCardActionArea,
+    Card as MuiCard,
+    CardMedia as MuiCardMedia,
+    CardContent as MuiCardContent,
+    CardActionArea as MuiCardActionArea,
 } from '@material-ui/core';
-
+import { device } from 'style';
 
 export const Card = withTheme(styled(MuiCard)`
-   height: 504px;
+    height: 430px;
+    
+    @media ${device.mobile} {
+        width: 270px;
+    }    
+    
+    @media ${device.tablet} {
+        width: 360px;
+    }
 `);
 
 export const CardMedia = withTheme(styled(MuiCardMedia)`
-   height: 60%;
+    position: relative;
+    height: 60%;
 `);
 
 
 export const CardActionArea = withTheme(styled(MuiCardActionArea)`
-   height: 100%;
-   &:focus {
-     outline: none;
-   }
+    height: 100%;
+
+    &:focus {
+        outline: none;
+    }
 `);
 
 export const CardContent = withTheme(styled(MuiCardContent)`
-   height: 40%;
-   font-family:  ${props => props.theme.fontFamily} !important;
-   > *:first-child {
-        font-size:  1.5rem !important;
-   }
-   > *:last-child {
-        font-size:  1rem !important;
-        color:  ${props => props.theme.palette.black} !important;
-   }
-   
+    height: 40%;
+    font-family: ${props => props.theme.fontFamily} !important;
+
+    > *:first-child {
+        font-size: 1.5rem !important;
+    }
+
+    > *:last-child {
+        font-size: 1rem !important;
+        color: ${props => props.theme.palette.black} !important;
+    }
+
 `);
