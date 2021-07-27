54bcd7cc1c4d8fd3102245c555058e53136c7de9
ofir.katz <ofir.katz@plataine.com>
2021-07-27 20:35:39 +0300

    - added mobile mobile device min-width

--- client/src/style/index.js
+++ client/src/style/index.js
@@ -8,11 +8,13 @@ export const colors = {
 };
 
 const size = {
+	mobile: '320px',
 	tablet: '768px',
 	laptop: '1024px',
 };
 
 export const device = {
+	mobile: `(min-width: ${size.mobile})`,
 	tablet: `(min-width: ${size.tablet})`,
 	laptop: `(min-width: ${size.laptop})`,
 };
