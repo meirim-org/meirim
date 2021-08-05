f771139789595819bacfd77b71bd333e76aea49e
Michal Mart <michal.mart@gmail.com>
2021-08-05 11:33:24 -0400

    update tests to rename isTag function

--- server/tests/integration/process/tags.test.js
+++ server/tests/integration/process/tags.test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const { Bookshelf, Knex} = require('../../../api/service/database');
-const { generateTagsForPlan, isTag } = require('../../../api/lib/tags');
+const { generateTagsForPlan } = require('../../../api/lib/tags');
+const { isTagByUsageAddition } = require('../../../api/lib/tags/utils');
 const { tags, tagDataRules } = require('../../../api/constants');
 const PlanAreaChanges = require('../../../api/model/plan_area_changes');
 
@@ -26,7 +26,7 @@ describe('Tags', function() {
 
 
  
-    describe('isTag helper function - housing tag tests', function() { 
+    describe('isTagByUsageAddition helper function - housing tag tests', function() { 
         let myStub;
         afterEach(async function() {
             myStub.restore();
@@ -34,49 +34,49 @@ describe('Tags', function() {
         });
 
 
-        it('returns housing tag from isTag for housingByUnits if database record is found & # of units exceeds minimum', async function() {
+        it('returns housing tag from isTagByUsageAddition for housingByUnits if database record is found & # of units exceeds minimum', async function() {
             myStub = sinon.stub(PlanAreaChanges,'byPlanAndUsage').returns(fakeHousingByUnitsTrue);
-            const result =  await isTag(planId,housingTag,HOUSING_BY_UNIT); 
+            const result =  await isTagByUsageAddition(planId,housingTag,HOUSING_BY_UNIT); 
             expect(result.tag_id).to.eql('1');
             expect(result.created_by_data_rules).to.eql(`{rule:'${tagDataRules[HOUSING_BY_UNIT].description}',detail:'adds +${fakeUnitsAdded} מגורים (יח"ד)'}`);
             myStub.restore();            
         });
 
-        it('does not return housing tag from isTag for housingByUnits if number of units doesn\'t meet minimum', async function() {
+        it('does not return housing tag from isTagByUsageAddition for housingByUnits if number of units doesn\'t meet minimum', async function() {
             const fakeUnitsAddedTooSmall = 5;
             const fakeHousingByUnitsTooSmall = { models: [{ attributes : { change_to_approved_state: `+${fakeUnitsAddedTooSmall}` } }]} ;
             myStub = sinon.stub(PlanAreaChanges,'byPlanAndUsage').returns(fakeHousingByUnitsTooSmall);
-            const result =  await isTag(planId,housingTag,HOUSING_BY_UNIT); 
+            const result =  await isTagByUsageAddition(planId,housingTag,HOUSING_BY_UNIT); 
             expect(result).to.eql(undefined);
             myStub.restore();            
         }); 
         
-        it('does not return housing tag from isTag for housingByUnits if database record is not found', async function() {
+        it('does not return housing tag from isTagByUsageAddition for housingByUnits if database record is not found', async function() {
             myStub = sinon.stub(PlanAreaChanges,'byPlanAndUsage').returns(null);
-            const result =  await isTag(planId,housingTag,HOUSING_BY_UNIT); 
+            const result =  await isTagByUsageAddition(planId,housingTag,HOUSING_BY_UNIT); 
             expect(result).to.eql(undefined);
             myStub.restore();            
         });           
 
-        it('returns housing tag from isTag for housingByArea if database record is found & area added is larger than minimum', async function() {
+        it('returns housing tag from isTagByUsageAddition for housingByArea if database record is found & area added is larger than minimum', async function() {
             myStub = sinon.stub(PlanAreaChanges,'byPlanAndUsage').returns(fakeHousingByAreaTrue);
-            const result =  await isTag(planId,housingTag,HOUSING_BY_AREA); 
+            const result =  await isTagByUsageAddition(planId,housingTag,HOUSING_BY_AREA); 
             expect(result.tag_id).to.eql('1');
             expect(result.created_by_data_rules).to.eql(`{rule:'${tagDataRules[HOUSING_BY_AREA].description}',detail:'adds +${fakeSqMrAdded} מגורים (מ"ר)'}`);
         });    
 
-        it('does not return housing tag from isTag for housingByArea if area added doesn\'t meet minimum', async function() {
+        it('does not return housing tag from isTagByUsageAddition for housingByArea if area added doesn\'t meet minimum', async function() {
             const fakeAreaAddedTooSmall = 500;
             const fakeHousingByAreaTooSmall = { models: [{ attributes : { change_to_approved_state: `+${fakeAreaAddedTooSmall}` } }]} ;
             myStub = sinon.stub(PlanAreaChanges,'byPlanAndUsage').returns(fakeHousingByAreaTooSmall);
-            const result =  await isTag(planId,housingTag,HOUSING_BY_AREA); 
+            const result =  await isTagByUsageAddition(planId,housingTag,HOUSING_BY_AREA); 
             expect(result).to.eql(undefined);
             myStub.restore();            
         }); 
         
-        it('does not return housing tag from isTag for housingByArea if database record is not found', async function() {
+        it('does not return housing tag from isTagByUsageAddition for housingByArea if database record is not found', async function() {
             myStub = sinon.stub(PlanAreaChanges,'byPlanAndUsage').returns(null);
-            const result =  await isTag(planId,housingTag,HOUSING_BY_AREA); 
+            const result =  await isTagByUsageAddition(planId,housingTag,HOUSING_BY_AREA); 
             expect(result).to.eql(undefined);
             myStub.restore();            
         });           
