6ec32df9dc604563134ac62d4cd8c1462f31fc45
Michal Mart <michal.mart@gmail.com>
2021-08-05 21:33:29 -0400

    refactor generateTagsForPlan to remove eval usage and fix tests

--- server/api/lib/tags/index.js
+++ server/api/lib/tags/index.js
@@ -1,17 +1,26 @@
-const Mustache = require('mustache');
 const { tags } = require('../../constants');
 const { isHousing } = require('../tags/housing');
+const Exception = require('../../../api/model/exception');
+const functions = {'isHousing':isHousing};
 
 const generateTagsForPlan = async (planId) => {
-    const planTags = [];
-    
-    for (const {tagId: id, tagName: name, tagHelperFunction: helperFunction} of tags) {
-        const result = await eval(Mustache.render(helperFunction, { tagId: id, planId: planId }));
-        planTags.push(...result);
-		return planTags;
-    } 
+	const planTags = [];
+	for (const tagCounter in tags) {
+		const { tagId, tagName,functionName}  = tags[tagCounter];
+		try {
+			const tagFunction = functions[functionName];
+			if (typeof(tagFunction) != "function") {
+				throw new Exception.BadRequest(`"${functionName}" must be a function that determines if tag "${tagName}" applies. Please require it and add it to the functions object`);
+			};
+			const result = await tagFunction(planId, tagId);
+			planTags.push(...result);
+		} catch (err) {
+			console.debug(err);			
+		}
+	} 
+	return planTags;	
 }
 
 module.exports = {
-    generateTagsForPlan
+	generateTagsForPlan
 };
