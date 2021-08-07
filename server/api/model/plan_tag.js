a10f951a18ab2b433c5a7e0d99fe0bd0a058d924
Michal Mart <michal.mart@gmail.com>
2021-08-07 14:26:53 -0400

    fix indentation

--- server/api/model/plan_tag.js
+++ server/api/model/plan_tag.js
@@ -7,12 +7,12 @@ class PlanTag extends Model {
 	get rules () {
 		return {
 			tag_id: ['required', 'integer'],
-        	plan_id: ['required', 'integer'],
+			plan_id: ['required', 'integer'],
 			display_score: ['integer'],
 			created_by_data_rules: ['string'],
 			created_by_child: ['boolean'],
 			child_is_stand_alone: ['boolean'],
-        	creation_date: ['datetime']
+			creation_date: ['datetime']
 		}
 
 	}
@@ -54,7 +54,7 @@ class PlanTag extends Model {
 					await new PlanTag(data[datum]).save(null, {transacting: transaction});
 			}
 		});
-		} catch (err){
+		} catch (err) {
 			console.log(err);
 		}
 
