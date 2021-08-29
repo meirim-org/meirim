3334ec9da5ba0b76301abe89ee9543bffe394776
Michal Mart <michal.mart@gmail.com>
2021-08-29 14:38:35 -0400

    format: replace spaces w tabs

--- server/migrations/20210825175016_init_tags_table.js
+++ server/migrations/20210825175016_init_tags_table.js
@@ -6,25 +6,25 @@ const employmentTag = require('../api/lib/tags/employment');
 
 exports.up = async function(knex) {
 
-    await knex.table('plan_tag').del();
+	await knex.table('plan_tag').del();
 
-    await knex.raw('ALTER TABLE tag DROP FOREIGN KEY tag_parent_id_foreign');
-    await knex.raw('ALTER TABLE tag DROP COLUMN parent_id');
+	await knex.raw('ALTER TABLE tag DROP FOREIGN KEY tag_parent_id_foreign');
+	await knex.raw('ALTER TABLE tag DROP COLUMN parent_id');
 
-    await knex.schema.table('tag', table => {
-        table.integer('parent_id')
-            .unsigned()
-            .references('id')
-            .inTable('tag')
-            .onDelete('set null');
-    });
+	await knex.schema.table('tag', table => {
+		table.integer('parent_id')
+			.unsigned()
+			.references('id')
+			.inTable('tag')
+			.onDelete('set null');
+	});
 
-    const tbl = knex.table('tag');
-    // remove everything from the table
-    await tbl.del();
+	const tbl = knex.table('tag');
+	// remove everything from the table
+	await tbl.del();
 
-    await tbl.insert({name: housingTag.TAG_NAME});
-    await tbl.insert({name: publicTag.TAG_NAME});
+	await tbl.insert({name: housingTag.TAG_NAME});
+	await tbl.insert({name: publicTag.TAG_NAME});
 	await tbl.insert({name: hotelieryTag.TAG_NAME});
 	await tbl.insert({name: commerceTag.TAG_NAME});
 	await tbl.insert({name: employmentTag.TAG_NAME});
@@ -32,12 +32,12 @@ exports.up = async function(knex) {
 };
 
 exports.down = async function(knex) {
-    const tbl = knex.table('tag');
+	const tbl = knex.table('tag');
 
-    await tbl.where('name', housingTag.TAG_NAME).del();
-    await tbl.where('name', publicTag.TAG_NAME).del();
+	await tbl.where('name', housingTag.TAG_NAME).del();
+	await tbl.where('name', publicTag.TAG_NAME).del();
 	await tbl.where('name', hotelieryTag.TAG_NAME).del();
 	await tbl.where('name', commerceTag.TAG_NAME).del();
 	await tbl.where('name', employmentTag.TAG_NAME).del();
-    
+	
 };
