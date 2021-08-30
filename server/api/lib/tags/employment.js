62d6d7f6a851492aedb7b14827426d3bf681d705
Michal Mart <michal.mart@gmail.com>
2021-08-30 09:15:49 -0400

    rename employment tag

--- server/api/lib/tags/employment.js
+++ server/api/lib/tags/employment.js
@@ -2,7 +2,7 @@
 // (request to build a completely new employment area with a size bigger than 200 mr [not an addition to an existing one]) 
 // or (request to build more than 2000 mr of employment).
 const { doesTagApplyHelper} = require('../tags/utils');
-const TAG_NAME = 'תעסוקה ותעשיה';
+const TAG_NAME = 'תעסוקה';
 
 const doesTagApply = async (planId, tagsResources) => {  
 	return doesTagApplyHelper(planId,TAG_NAME, tagsResources);
