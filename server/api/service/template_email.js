b59687b1aee00b924e45a8d27a06aeb6db220f9d
Gal Gendler <ggendler@gmail.com>
2021-09-08 07:47:59 +0300

    fix

--- server/api/service/template_email.js
+++ server/api/service/template_email.js
@@ -26,18 +26,17 @@ class DynamicTemplateEmail {
 			 DigestPlanAlert: 'd-db0b55ab042e4ba9878715acd35c1d6c'
 		 };
  
-		 sgMail.setApiKey('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');
 	 }
 
 	 digestPlanAlert (emailPlanParams, emailAlertParams, recipient) {
 		 const email = {
 			 from: `"${this.config.from_name}" <${this.config.from_email}>`, // sender address
-			 to: 'ggendler@gmail.com',
+			 to: 'eyal.mig@gmail.com',
 			unsubscribeLink: `${this.baseUrl}${emailAlertParams.unsubscribeLink}`,
 			 personalizations:[
 				 {
 					 'to':[{
-						 'email': 'ggendler@gmail.com'
+						 'email': 'eyal.mig@gmail.com'
 					 }
 					 ],
 					'dynamic_template_data': {
