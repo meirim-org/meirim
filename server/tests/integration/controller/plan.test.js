e39f20cbddcfe3bf7b339fa9506e388df7fe9f99
TomerMe2 <tomer.laor1@gmail.com>
2021-09-15 00:30:07 +0300

    change order of db tables in test

--- server/tests/integration/controller/plan.test.js
+++ server/tests/integration/controller/plan.test.js
@@ -3,7 +3,7 @@ const { mockDatabase } = require('../../mock');
 const { Plan } = require('../../../api/model');
 
 describe('Plan controller', function() {
-	const tables = ['alert', 'plan', 'notification', 'person', 'plan_person', 'plan_tag', 'tag'];
+	const tables = ['alert', 'plan', 'notification', 'person', 'plan_person', 'tag', 'plan_tag'];
 	beforeEach(async function() {
 		await mockDatabase.createTables(tables);
 	});
