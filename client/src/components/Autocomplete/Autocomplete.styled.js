248c0eb78efe0f7e7dd95896b3e7f4972a26ad13
winteromi <winteromi@gmail.com>
2021-09-23 19:42:54 +0300

    issue 396 - Autocomplete, FilteBox and InfoBadge and PlansSearchBar components

--- client/src/components/Autocomplete/Autocomplete.styled.js
+++ client/src/components/Autocomplete/Autocomplete.styled.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
-import { colors } from 'style';
+import { colors, device } from 'style';
+
+export const Wrapper = styled.div`
+    width: 100%;
+    position: relative;
+
+    @media ${device.tablet} {
+        width: ${props => props.width};
+    }
+`
 
 export const Menu = styled.ul`
 border: 1px solid ${colors.purple[500]};
@@ -7,7 +16,7 @@ border-top: none;
 border-radius: 0px 0px 4px 4px;
 position: absolute;
 top: 41px;
-z-index: 0;
+z-index: 1000;
 width: 100%;
 padding: 0;
 `
@@ -23,7 +32,7 @@ border: 1px solid ${colors.purple[500]};
 border-radius: 0px 4px 4px 0px;
 padding: 0 15px;
 background-color: ${colors.white};
-z-index: 1;
+z-index: 1001;
 `
 
 export const Button = styled.button`
@@ -32,7 +41,7 @@ border: none;
 border-radius: 4px 0px 0px 4px;
 background: ${colors.purple[500]};
 width: 44px;
-z-index: 1;
+z-index: 1001;
 cursor: pointer;
     &:focus{
         outline: none;
---
+++ client/src/components/InfoBadge/InfoBadge.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import { withStyles } from '@material-ui/core/styles';
+import { ReactComponent as QuestionMarkIcon } from 'assets/svg/question-mark.svg';
+import { BadgeWrapper } from './InfoBadge.styled';
+import { colors } from 'style';
+
+const StyledTooltip = withStyles(() => ({
+    arrow: {
+        color: colors.black,
+    },
+    tooltip: {
+        backgroundColor: colors.black,
+        fontSize: '14px',
+        fontFamily: 'Assistant',
+        padding: '9px 11px',
+        textAlign: 'center',
+        whiteSpace: 'pre-line'
+    },
+}))(Tooltip);
+
+function InfoBadge(props) {
+    const { text } = props;
+    return (<StyledTooltip title={text} arrow>
+        <BadgeWrapper>
+            <QuestionMarkIcon />
+        </BadgeWrapper>
+    </StyledTooltip>);
+}
+
+export default InfoBadge;
