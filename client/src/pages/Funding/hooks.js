247d18f91c1b240c47db3641b660e436229290a9
Gal Gendler <ggendler@gmail.com>
2021-12-05 20:45:02 +0200

    added ref in location to go to payment

--- client/src/pages/Funding/hooks.js
+++ client/src/pages/Funding/hooks.js
@@ -62,3 +62,15 @@ export const useWhoWeAreAnchor = (locationHash, whoWeAreRef) => {
 		}
 	}, [locationHash, whoWeAreRef]);
 };
+
+export const usePaymentAnchor = (locationHash, paymentRef) => {
+	useEffect(() => {
+		if (locationHash === '#payment') {
+			// scrollIntoView isn't sufficient since the header hides some of the view
+			const y = paymentRef.current.getBoundingClientRect().top - 150;
+			window.scrollBy({top: y, behavior: 'smooth'});
+		} else {
+			window.scroll({top: 0, behavior: 'smooth'});
+		}
+	}, [locationHash, paymentRef]);
+};
