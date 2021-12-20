6e158601fe74f07c7fd40ce7e571bc8c71698105
Gal Gendler <ggendler@gmail.com>
2021-12-20 21:48:10 +0200

    added support for open url

--- client/src/pages/Homepage/index.jsx
+++ client/src/pages/Homepage/index.jsx
@@ -1,11 +1,26 @@
 import Wrapper from 'components/Wrapper';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { WeNeedYou } from 'shared';
 import CommunityDiscussions from './CommunityDiscussions';
 import TopSection from './TopSection';
 import TopViews from './TopViews';
+import { openModal } from 'redux/modal/slice';
+import { UserSelectors } from 'redux/selectors';
 
 const Homepage = () => {
+
+	const location = useLocation();
+	const dispatch = useDispatch();
+	const { isAuthenticated } = UserSelectors();
+
+	useEffect(() => {
+		if (location.hash === '#openRegister' &&  !isAuthenticated) {
+			dispatch(openModal({ modalType: 'register' }));
+		}
+	}, [location, dispatch]);
+
 	return (
 		<Wrapper>
 			<TopSection />
