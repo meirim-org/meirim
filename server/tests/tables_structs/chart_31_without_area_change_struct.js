067029169106122924a96770e672c8c28849821e
Tomer Laor <tomer.laor1@gmail.com>
2022-01-10 23:50:21 +0200

    fix size of integer in test tables structs

--- server/tests/tables_structs/chart_31_without_area_change_struct.js
+++ server/tests/tables_structs/chart_31_without_area_change_struct.js
@@ -1,6 +1,6 @@
 const Chart31WithoutAreaChangeStruct = function(t) {
 	t.increments();
-	t.integer('plan_id', 65535);
+	t.integer('plan_id');
 	t.text('designation', 65535);
 	t.text('cells', 65535);
 	return t;
