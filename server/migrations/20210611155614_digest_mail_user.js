7d29c2727133d5858ee7aeec7693860456f50701
Gal Gendler <ggendler@gmail.com>
2022-02-20 21:30:37 +0200

    work on dges

--- server/migrations/20210611155614_digest_mail_user.js
+++ server/migrations/20210611155614_digest_mail_user.js
@@ -3,10 +3,18 @@ exports.up = async function(knex) {
 	await knex.schema.table('alert', table => {
 		table.datetime('last_email_sent');
 	});
+
+	await knex.schema.createTableIfNotExists('staticmap', t => {
+        t.increments('id').primary();
+        t.integer('plan_id');
+		t.text('base64string', 'longtext');
+    });
 };
 
 exports.down = function(knex) {
-	return knex.schema.table('alert', table => {
+	await knex.schema.table('alert', table => {
 		table.dropColumns('last_email_sent');
 	});
+
+	await knex.schema.dropTableIfExists('staticmap');
 };
