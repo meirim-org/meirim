f821c9f43bdd450aaec525f6e096709092d2d507
Gal Gendler <ggendler@gmail.com>
2022-02-22 20:39:32 +0200

    poptin

---
+++ server/migrations/20220117211500_last_visited_status.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+	return knex.schema.table('plan', table => {
+		
+		table.dropColumns(
+			'last_status_update'		
+		);
+		table.timestamp('last_visited_status').defaultTo(knex.fn.now());
+	});
+};
+
+exports.down = function(knex) {
+	return knex.schema.table('plan', table => {
+		table.timestamp('last_status_update', );
+		table.dropColumns(
+			'last_visited_status'		
+		);
+	}).then(() => knex('plan').update('last_status_update', knex.ref('updated_at')));
+};
