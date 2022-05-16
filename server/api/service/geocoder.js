50f795a55ef86951ccf770804197bb57477db25f
gruppin <gruppin@gmail.com>
2022-05-16 20:12:36 +0300

    catch errors (#527)
    * catch errors
    
    * async fix

--- server/api/service/geocoder.js
+++ server/api/service/geocoder.js
@@ -1,6 +1,7 @@
 const NodeGeocoder = require('node-geocoder');
 const Config = require('../lib/config');
 const axios = require('axios');
+const Log = require('../lib/log');
 
 const geocoder = NodeGeocoder(Config.get('geocoder'));
 
@@ -20,36 +21,42 @@ const degreeToMeter = (lon, lat, dn, de) => {
 
 
 // Some parcels (Gushim) are malformed (usually in the Golan Heights) and should be ignored.
-const BAD_PARCELS = ['203000','202011','202000', '201000', '200000', '200001'];
+const BAD_PARCELS = ['203000', '202011', '202000', '201000', '200000', '200001'];
 
 const gushHelkaToPolygon = async (gush, helka) => {
-	if (!gush || ! helka){
+	if (!gush || !helka) {
 		return;
 	}
 	if (BAD_PARCELS[gush]) {
 		return;
 	}
-	const res = await axios(
-		{ 
-			url :`https://open.govmap.gov.il/geoserver/opendata/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=opendata:PARCEL_ALL&TYPENAME=opendata:PARCEL_ALL&outputFormat=json&cql_FILTER=GUSH_NUM=${gush}AND%20PARCEL=${helka}&srsName=EPSG:4326`,
-			method: 'get',
-			timeout: 20000,
-		});
-	const geoJsonRes = res.data;
+	try {
+		const res = await axios(
+			{
+				url: `https://open.govmap.gov.il/geoserver/opendata/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=opendata:PARCEL_ALL&TYPENAME=opendata:PARCEL_ALL&outputFormat=json&cql_FILTER=GUSH_NUM=${gush}AND%20PARCEL=${helka}&srsName=EPSG:4326`,
+				method: 'get',
+				timeout: 20000,
+			});
+		const geoJsonRes = res.data;
 
-	if (!geoJsonRes || !geoJsonRes['features'] || !geoJsonRes['features'][0] || !geoJsonRes['features'][0]['geometry']) {
-		console.log('Error in geoJson:', geoJsonRes);
+		if (!geoJsonRes || !geoJsonRes['features'] || !geoJsonRes['features'][0] || !geoJsonRes['features'][0]['geometry']) {
+			Log.error('Error in geoJson:', geoJsonRes);
+			return;
+		}
+		Log.info('Gush Helka polygon is:', JSON.stringify(geoJsonRes['features'][0]['geometry']));
+		const geometryRes = geoJsonRes['features'][0]['geometry'];
+		if (geometryRes['type'] == 'Polygon') {
+			return geometryRes;
+		}
+		else if (geometryRes['type'] == 'MultiPolygon') {
+			return { type: 'polygon', coordinates: geometryRes.coordinates[0] };
+		}
+		else return geometryRes;
+	}
+	catch (err) {
+		Log.error('could not enrich with gush helka: ',err.message);
 		return;
 	}
-	console.log('Gush Helka polygon is:',JSON.stringify( geoJsonRes['features'][0]['geometry'] ));
-	const geometryRes = geoJsonRes['features'][0]['geometry'];
-	if (geometryRes['type'] == 'Polygon') {
-		return geometryRes;
-	}
-	else if (geometryRes['type'] == 'MultiPolygon'){
-		return { type: 'polygon', coordinates: geometryRes.coordinates[0] };
-	}
-	else return geometryRes;
 };
 
 
