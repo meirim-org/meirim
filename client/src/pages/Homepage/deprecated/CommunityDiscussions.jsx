d621f51e840d3a02a8e3c4a5a5fa945378f40375
Gal Gendler <ggendler@gmail.com>
2022-06-08 16:12:15 +0300

    legacy stuff to folder

--- client/src/pages/Homepage/deprecated/CommunityDiscussions.jsx
+++ client/src/pages/Homepage/deprecated/CommunityDiscussions.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { H2, CommonSection } from './style';
+import { H2, CommonSection } from '../style';
 import Preview from './Preview';
 import communityDiscussionsImage from '../../assets/community-discussions.svg';
 import communityDiscussionsImageMobile from '../../assets/community-discussions-mobile.svg';
