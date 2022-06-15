7535eb9a81024031b848227341e7d1084f96a3fd
Gal Gendler <ggendler@gmail.com>
2022-06-15 15:55:21 +0300

    chip styles

--- client/src/shared/chip/index.jsx
+++ client/src/shared/chip/index.jsx
@@ -8,11 +8,10 @@ const StyledChip = styled.div`
   line-height: 21px;
   text-align: center;
   color: #4D20B2;
-  background-color: #F5F5F5;
   border: 1px solid #4D20B2;
   border-radius: 100px;
   padding: 8px 16px;
-  margin: 20px 10px;
+  cursor: pointer;
 `;
 
 const Chip = ({ text }) => (
