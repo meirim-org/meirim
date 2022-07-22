7832a618d73de8e574150266872ad2936cc4d276
Gal Gendler <ggendler@gmail.com>
2022-07-22 21:44:47 +0300

    fix linting

--- client/src/pages/Plan/common/Panels/stats.jsx
+++ client/src/pages/Plan/common/Panels/stats.jsx
@@ -11,7 +11,7 @@ import * as SC from './style';
 export const StatsPanel = ({ dataArea, textArea }) => {
 	const { t } = useTranslation();
 	const theme = useTheme();
-	if (!dataArea || !dataArea[0] || !dataArea[0].data.length || (dataArea[0].data[0].y ==0 && dataArea[1].data[0].y ==0 )) return null;
+	if (!dataArea || !dataArea[0] || !dataArea[0].data.length || (dataArea[0].data[0].y ===0 && dataArea[1].data[0].y ===0 )) return null;
 
 	return (
 		<TabPanel>
