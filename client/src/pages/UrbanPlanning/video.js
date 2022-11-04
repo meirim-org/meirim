47d612676933be03ef316615afd51f946bee90fe
Gal Gendler <ggendler@gmail.com>
2022-11-04 17:24:53 +0200

    removed merchav banner code

--- client/src/pages/UrbanPlanning/video.js
+++ client/src/pages/UrbanPlanning/video.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import YoutubeVideo from 'react-youtube';
 import styled from 'styled-components';
-import merchav from 'assets/merchav-logo.svg';
 
 const VideoWrapper = styled.div`
     /* position: relative; */
@@ -15,47 +14,47 @@ const VideoWrapper = styled.div`
     }
 `;
 
-const VideoBanner = styled.div`
-    position: absolute;
-    width: 509px;
-    height: 144px;
-    display: flex;
-    justify-content: space-between;
-    background: linear-gradient(0deg, #652dd0, #652dd0);
-    bottom: -222px;
-    box-shadow: 0px 29.6621px 147.057px rgba(0, 0, 0, 0.0503198),
-        0px 15.8588px 78.6238px rgba(0, 0, 0, 0.0417275),
-        0px 8.8903px 44.0759px rgba(0, 0, 0, 0.035),
-        0px 4.72157px 23.4084px rgba(0, 0, 0, 0.0282725);
-    border-radius: 12px;
-    padding: 24px 32px;
-`;
+// const VideoBanner = styled.div`
+//     position: absolute;
+//     width: 509px;
+//     height: 144px;
+//     display: flex;
+//     justify-content: space-between;
+//     background: linear-gradient(0deg, #652dd0, #652dd0);
+//     bottom: -222px;
+//     box-shadow: 0px 29.6621px 147.057px rgba(0, 0, 0, 0.0503198),
+//         0px 15.8588px 78.6238px rgba(0, 0, 0, 0.0417275),
+//         0px 8.8903px 44.0759px rgba(0, 0, 0, 0.035),
+//         0px 4.72157px 23.4084px rgba(0, 0, 0, 0.0282725);
+//     border-radius: 12px;
+//     padding: 24px 32px;
+// `;
 
-const VideoBannerSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    color: #ffffff;
-`;
+// const VideoBannerSection = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     color: #ffffff;
+// `;
 
-const InCollaboration = styled.div`
-    img {
-        margin-right: 12px;
-        margin-bottom: 16px;
-    }
-`;
+// const InCollaboration = styled.div`
+//     img {
+//         margin-right: 12px;
+//         margin-bottom: 16px;
+//     }
+// `;
 
-const CourseButton = styled.div`
-    background: #ffffff;
-    border-radius: 4px;
-    width: 120px;
-    height: 32px;
-    color: #000000;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-self: flex-end;
-    cursor: pointer;
-`;
+// const CourseButton = styled.div`
+//     background: #ffffff;
+//     border-radius: 4px;
+//     width: 120px;
+//     height: 32px;
+//     color: #000000;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     align-self: flex-end;
+//     cursor: pointer;
+// `;
 
 export default function video({ youtubeId }) {
     return (
@@ -77,7 +76,7 @@ export default function video({ youtubeId }) {
                         <img src={merchav} alt="merchav" />
                     </InCollaboration>
                     <CourseButton>לקורס המלא</CourseButton>
-                </VideoBannerSection>
+                </VideoBVideoBannerannerSection>
             </VideoBanner> */}
         </VideoWrapper>
     );
