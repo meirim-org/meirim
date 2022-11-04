718366d081e346cf24486f8dd3a2fbb3b4c829a3
Gal Gendler <ggendler@gmail.com>
2022-11-04 17:22:03 +0200

    removed reduandant device

--- client/src/pages/UrbanPlanning/TopSection.js
+++ client/src/pages/UrbanPlanning/TopSection.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'locale/he_IL';
-import { device } from 'style';
 
 const Section = styled.section`
     display: flex;
