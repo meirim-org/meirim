6d842364b9bb6113c24c59c72b3d0377bb7ed461
harshitsan <harshitbhalla.hb@gmail.com>
2022-11-21 14:24:38 +0530

    fix wa message

--- client/src/pages/Tree/common/Views/shareTree.jsx
+++ client/src/pages/Tree/common/Views/shareTree.jsx
@@ -13,14 +13,14 @@ const ShareWrapper = styled.div`
     max-width: 100%;
     min-width: 21.5rem;
     @media ${device.tablet} {
-        min-width: 31.5rem; 
+        min-width: 31.5rem;
     }
 `;
 
-const ShareTitleWrapper =  withTheme(styled.div`
+const ShareTitleWrapper = withTheme(styled.div`
     text-align: center;
-    border-bottom: 1px solid ${props => props.theme.palette.gray['radio']};
-    
+    border-bottom: 1px solid ${(props) => props.theme.palette.gray['radio']};
+
     padding-bottom: 1rem;
     @media ${device.tablet} {
         padding-bottom: 2.5rem;
@@ -28,22 +28,22 @@ const ShareTitleWrapper =  withTheme(styled.div`
     > * {
         font-size: 24px !important;
         @media ${device.tablet} {
-           font-size: 32px !important;
-           font-weight: 600 !important;
+            font-size: 32px !important;
+            font-weight: 600 !important;
         }
     }
 `);
 
-const ShareActionWrapper =  withTheme(styled.div`
+const ShareActionWrapper = withTheme(styled.div`
     padding: 1.5rem 2.2rem;
     text-align: center;
     @media ${device.tablet} {
-       padding: 2rem 3.75rem;
+        padding: 2rem 3.75rem;
     }
     > * {
         font-size: 16px !important;
         @media ${device.tablet} {
-           font-size: 14px !important;
+            font-size: 14px !important;
         }
     }
 `);
@@ -52,10 +52,13 @@ const ShareButtonWrapper = withTheme(styled.div`
     margin-bottom: 1rem;
     .MuiButton-root {
         width: 100%;
-        border: transparent!important;
-        background-color: ${props => props.theme.palette.green['whatsapp']} !important;
-        &:hover, &:focus {
-            background-color: ${props => props.theme.palette.green['whatsapp']} !important;
+        border: transparent !important;
+        background-color: ${(props) =>
+            props.theme.palette.green['whatsapp']} !important;
+        &:hover,
+        &:focus {
+            background-color: ${(props) =>
+                props.theme.palette.green['whatsapp']} !important;
             outline: 0 !important;
         }
         min-height: 3rem;
@@ -67,17 +70,16 @@ const ShareButtonWrapper = withTheme(styled.div`
     .MuiButton-label {
         text-transform: capitalize;
     }
-
 `);
 
 const ShareTextWrapper = styled.div`
     margin-bottom: 1rem;
     text-align: center;
-    
+
     > * {
         font-size: 12px !important;
         @media ${device.tablet} {
-           font-size: 14px !important;
+            font-size: 14px !important;
         }
     }
 `;
@@ -86,65 +88,69 @@ const CopyUrlArea = withTheme(styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    background-color: ${props => props.theme.palette.gray['bg']} !important;
+    background-color: ${(props) => props.theme.palette.gray['bg']} !important;
     align-items: center;
     padding: 1rem;
     border-radius: 12px;
-    border: solid 1px ${props => props.theme.palette.gray['400']} !important;
+    border: solid 1px ${(props) => props.theme.palette.gray['400']} !important;
 
     > span {
         direction: ltr;
         white-space: nowrap;
         overflow-x: scroll;
-        font-size: 12px !important;  
-        max-width: 11.8rem;              
+        font-size: 12px !important;
+        max-width: 11.8rem;
         @media ${device.tablet} {
-           max-width: 17rem; 
-           font-size: 14px !important;
-        }        
+            max-width: 17rem;
+            font-size: 14px !important;
+        }
     }
 `);
 
 const ShareTreeView = () => {
-	const theme = useTheme();
-	const url = encodeURI(window.location.toString());
-	const whatsappURL = `https://wa.me/?text=היתר%20שאולי%יעניין%20אותך%3A%0A${url}`;
+    const theme = useTheme();
+    const url = encodeURI(window.location.toString());
+    const whatsappURL = `https://wa.me/?text=רשיון%20כריתת%20עצים%20שאולי%20יעניין%20אותך%3A%0A${url}`;
     const { t } = useTranslation();
 
-	return (
-		<ShareWrapper>
-			<ShareTitleWrapper>
-				<Text text={t.shareTree} color={theme.palette.primary['800']}/>
-			</ShareTitleWrapper>
-			<ShareActionWrapper>
-				<ShareButtonWrapper>
-					<Button
-					 component="a"
-					 href={whatsappURL}
-					 target="_blank"
-					 text={t.whatsappShare} 
-					 fontWeight="600" 
-					 textcolor={theme.palette.white}
-					 iconBefore={<WhatsAppIcon/>} />
-				</ShareButtonWrapper>
-				<ShareTextWrapper>
-					<Text text={t.copyUrl} color={theme.palette.black}/>
-				</ShareTextWrapper>
-				<CopyUrlArea>
-					<Text text={url} size="14px" color={theme.palette.black}/>
-					<CopyToClipboard text={url} onCopy={() => copiedToClipboard()}>
-						<Button
-						 text={t.copy} 
-						 fontSize="14px" 
-						 fontWeight="400" 
-						 textcolor={theme.palette.primary.main}
-						 simple={true}
-						 />
-					</CopyToClipboard>
-				</CopyUrlArea>
-			</ShareActionWrapper>
-		</ShareWrapper>
-	);
+    return (
+        <ShareWrapper>
+            <ShareTitleWrapper>
+                <Text text={t.shareTree} color={theme.palette.primary['800']} />
+            </ShareTitleWrapper>
+            <ShareActionWrapper>
+                <ShareButtonWrapper>
+                    <Button
+                        component="a"
+                        href={whatsappURL}
+                        target="_blank"
+                        text={t.whatsappShare}
+                        fontWeight="600"
+                        textcolor={theme.palette.white}
+                        iconBefore={<WhatsAppIcon />}
+                    />
+                </ShareButtonWrapper>
+                <ShareTextWrapper>
+                    <Text text={t.copyUrl} color={theme.palette.black} />
+                </ShareTextWrapper>
+                <CopyUrlArea>
+                    <Text text={url} size="14px" color={theme.palette.black} />
+                    <CopyToClipboard
+                        text={url}
+                        onCopy={() => copiedToClipboard()}
+                    >
+                        <Button
+                            text={t.copy}
+                            fontSize="14px"
+                            fontWeight="400"
+                            textcolor={theme.palette.primary.main}
+                            simple={true}
+                        />
+                    </CopyToClipboard>
+                </CopyUrlArea>
+            </ShareActionWrapper>
+        </ShareWrapper>
+    );
 };
 
 export default ShareTreeView;
