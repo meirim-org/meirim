1727879d913c16a513cca768e0e7728ebf636b61
Gal Gendler <ggendler@gmail.com>
2022-12-02 10:02:00 +0200

    linting fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create(process.env.CONFIG.axios)
-// TESTING PURPOSE
-// const instance = axios.create({
-//     baseURL: 'https://a910-119-82-86-231.in.ngrok.io',
-// });
 
 export default {
     get: (path, data, options) =>
