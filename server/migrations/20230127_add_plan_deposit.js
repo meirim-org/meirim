b00768697a90688ebd1aca28432a785a16381238
yael alfasi <yaelal@gmail.com>
2023-01-30 23:31:06 +0200

    issue 637

--- server/migrations/20230127_add_plan_deposit.js
+++ server/migrations/20230127_add_plan_deposit.js
@@ -1,7 +1,9 @@
 exports.up = function(knex) {
     return knex.schema.table('plan', table => {
       table.boolean('was_deposited').default('false');
-    });
+    }).then(() => {
+      return knex.raw('UPDATE plan set was_deposited=true where id in (select distinct p.plan_id from plan_status_change p join status_mapping s on s.mavat_status = p.`status` and s.meirim_status in (\'תכנית מבוטלת\',\'תכנית מאושרת\'))');
+  });;
   };
 
 exports.down = async function(knex) {
