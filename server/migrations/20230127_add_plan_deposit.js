69a995cce1c5daa865b9de4475b6bf48cf613d82
yael alfasi <yaelal@gmail.com>
2023-07-03 13:03:45 +0300

    issue 637

--- server/migrations/20230127_add_plan_deposit.js
+++ server/migrations/20230127_add_plan_deposit.js
@@ -3,10 +3,19 @@ const {	meirimStatuses } = require('../api/constants');
 exports.up = function(knex) {
     return knex.schema.table('plan', table => {
       table.boolean('was_deposited').default('false');
-    }).then(() => {
-      return knex.raw('UPDATE plan set was_deposited=true where id in (select distinct p.plan_id from plan_status_change p join status_mapping s on s.mavat_status = p.`status` and s.meirim_status in (\''+meirimStatuses.CANCELLED+'\',\''+meirimStatuses.APPROVED+'\', \''+meirimStatuses.PUBLIC_OBJECTION+'\'))');
-  });
+    }).then(() => handle(knex));
   };
+
+async function handle(knex) {
+  for (var i = 0; i < 35000; i+=1000) {
+    var cmd = 'UPDATE plan set was_deposited=true where id in (select distinct p.plan_id from plan_status_change p join status_mapping s '+
+    'on s.mavat_status = p.`status` and p.plan_id >= '
+      +i+' and p.plan_id < '+(i+1000)+
+      ' and s.meirim_status in (\''+meirimStatuses.CANCELLED+'\',\''+meirimStatuses.APPROVED+'\', \''+meirimStatuses.PUBLIC_OBJECTION+'\'))';
+    console.error(cmd);
+    await knex.raw(cmd);
+  }
+}
 
 exports.down = async function(knex) {
     await knex.schema.table('plan', table => {
