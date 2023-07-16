f677abb3781c0bb715e25249538fb1be58df6d86
Roman Malyshev <microooji01@gmail.com>
2023-07-16 19:10:40 +0900

    fixed plans empty area

--- client/src/hooks.js
+++ client/src/hooks.js
@@ -5,8 +5,8 @@ import { useDispatch } from 'react-redux';
 import ReactGA from 'react-ga';
 import { hotjar } from 'react-hotjar';
 import { map } from 'lodash';
-import { authenticated, fetchedFavoritePlans } from 'redux/user/slice'; 
-import { closeModal } from 'redux/modal/slice'; 
+import { authenticated, fetchedFavoritePlans } from 'redux/user/slice';
+import { closeModal } from 'redux/modal/slice';
 import { HOME, ALERTS } from 'router/contants';
 import api from 'services/api';
 import { fetchUserPlans } from 'pages/UserPlans/controller';
@@ -16,40 +16,42 @@ export const ValidUserHook = (user) => {
 	const history = useHistory();
 	const isHomePage = history.location.pathname === '/';
 	useEffect(() => {
-		if (user){
+		if (user) {
 			dispatch(authenticated({ user }));
 			isHomePage && history.push(ALERTS);
 			dispatch(closeModal());
 		}
-	},[user, dispatch, history, isHomePage]);
+	}, [user, dispatch, history, isHomePage]);
 };
 
 export const CookieHook = () => {
 	const dispatch = useDispatch();
-	const [ loading, setLoading ] = useState(true);
-	const [ success, setSuccess ] = useState(false);
-	const [ response, setResponse ] = useState({});
-	const [ error, setError ] = useState({});
+	const [loading, setLoading] = useState(true);
+	const [success, setSuccess] = useState(false);
+	const [response, setResponse] = useState({});
+	const [error, setError] = useState({});
 	useEffect(() => {
-		api.get('/me').then((response) => {
-			const { name, id, admin } = response.me;
-			setSuccess(true);
-			dispatch(authenticated({ user: { name, id, admin } }));
-			setLoading(false);
-			setResponse(response);
-			return fetchUserPlans(id).then(({ data })=>{
-				const favoritePlans = map(data, 'id');
-				dispatch(fetchedFavoritePlans({ favoritePlans }));
-			});
-		}).catch((err) => {
-			setError(err);
-			setLoading(false);
-			setSuccess(false);
-		});
+		api.get('/me')
+			.then((response) => {
+				const { name, id, admin } = response.me;
+				setSuccess(true);
+				dispatch(authenticated({ user: { name, id, admin } }));
+				setLoading(false);
+				setResponse(response);
 
+				return fetchUserPlans(id).then(({ data }) => {
+					const favoritePlans = map(data, 'id');
+					dispatch(fetchedFavoritePlans({ favoritePlans }));
+				});
+			})
+			.catch((err) => {
+				setError(err);
+				setLoading(false);
+				setSuccess(false);
+			});
 	}, [dispatch]);
-	
-	return { success, response , error, loading };
+
+	return { success, response, error, loading };
 };
 
 export const CheckIfUserCanAccessPage = () => {
@@ -79,4 +81,42 @@ export const useInitHotjar = () => {
 			);
 		}
 	}, []);
+};
+
+export const useStickyPlansHeader = () => {
+	const [translateY, setTranslateY] = useState(0);
+	const [hiddenTopSection, setHiddenTopSection] = useState(false);
+	const [hiddenTopContentSection, setHiddenTopContentSection] =
+        useState(false);
+	const [lastScrollY, setLastScrollY] = useState(0);
+	const handleScrollEvent = () => {
+		if (lastScrollY > window.scrollY) {
+			setHiddenTopSection(false);
+			setTranslateY(-100);
+		} else if (lastScrollY < window.scrollY) {
+			setHiddenTopSection(true);
+			setHiddenTopContentSection(true);
+			setTranslateY(-200);
+		}
+		setLastScrollY(window.scrollY);
+		if (window.scrollY === 0) {
+			setHiddenTopSection(false);
+			setHiddenTopContentSection(false);
+			setTranslateY(0);
+		}
+	};
+
+	useEffect(() => {
+		console.log('lastScrollY', lastScrollY, window.scrollY);
+	}, [translateY]);
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScrollEvent);
+
+		return () => {
+			window.removeEventListener('scroll', handleScrollEvent);
+		};
+	});
+
+	return { translateY, hiddenTopSection, hiddenTopContentSection };
 };
