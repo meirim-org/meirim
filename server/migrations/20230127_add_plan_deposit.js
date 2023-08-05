64ff8389556c029da767c309eeb81c73cdb51058
Gal Gendler <ggendler@gmail.com>
2023-08-05 13:25:02 +0300

    fix

--- server/migrations/20230127_add_plan_deposit.js
+++ server/migrations/20230127_add_plan_deposit.js
@@ -1,20 +1,20 @@
-const {	meirimStatuses } = require('../api/constants');
+// const {	meirimStatuses } = require('../api/constants');
 
 exports.up = function(knex) {
-    return knex.schema.table('plan', table => {
-      return knex.schema.hasColumn('plan', 'was_deposited').then((exists) => {
-        if(!exists) {
-          table.boolean('was_deposited').default('false');  
-        }
-      });
-    }).then(() => {
-      // This should run when we're ready, by sets
-      // return knex.raw('UPDATE plan set was_deposited=true where id in (select distinct p.plan_id from plan_status_change p join status_mapping s on s.mavat_status = p.`status` and s.meirim_status in (\''+meirimStatuses.CANCELLED+'\',\''+meirimStatuses.APPROVED+'\', \''+meirimStatuses.PUBLIC_OBJECTION+'\'))');
-  });
+//     return knex.schema.table('plan', table => {
+//       return knex.schema.hasColumn('plan', 'was_deposited').then((exists) => {
+//         if(!exists) {
+//           return table.boolean('was_deposited').default('false');  
+//         }
+//       });
+//     }).then(() => {
+//       // This should run when we're ready, by sets
+//       // return knex.raw('UPDATE plan set was_deposited=true where id in (select distinct p.plan_id from plan_status_change p join status_mapping s on s.mavat_status = p.`status` and s.meirim_status in (\''+meirimStatuses.CANCELLED+'\',\''+meirimStatuses.APPROVED+'\', \''+meirimStatuses.PUBLIC_OBJECTION+'\'))');
+//   });
   };
 
 exports.down = async function(knex) {
-    await knex.schema.table('plan', table => {
-        table.dropColumns('was_deposited');
-    });
+//     await knex.schema.table('plan', table => {
+//         table.dropColumns('was_deposited');
+//     });
 };
