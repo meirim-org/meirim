415a4464493f132e56a97a35d5920f94746a70b3
Gal Gendler <ggendler@gmail.com>
2023-12-08 19:29:07 +0200

    test

--- client/src/pages/Funding/hooks.js
+++ client/src/pages/Funding/hooks.js
@@ -6,94 +6,94 @@ import { openModal } from 'redux/modal/slice';
 import { successPageTransactionCompleteMessage } from './constants';
 
 export const useStatsDataHandler = (paymentDone) => {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	useEffect(() => {
-		getFundingStats()
-			.then((stats) => {
-				dispatch(
-					setStatsData({
-						statsData: stats,
-					})
-				);
-			})
-			.catch((err) => {
-				console.error('get funding stats failed:', err);
-			});
-	}, [dispatch, paymentDone]);
+    useEffect(() => {
+        getFundingStats()
+            .then((stats) => {
+                dispatch(
+                    setStatsData({
+                        statsData: stats,
+                    })
+                );
+            })
+            .catch((err) => {
+                console.error('get funding stats failed:', err);
+            });
+    }, [dispatch, paymentDone]);
 };
 
 export const useSuccessCloseHandler = (paymentSuccessCb) => {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const handleMessage = useCallback(
-		(event) => {
-			try {
-				const data = JSON.parse(event.data);
+    const handleMessage = useCallback(
+        (event) => {
+            try {
+                const data = JSON.parse(event.data);
 
-				if (data.message === successPageTransactionCompleteMessage) {
-					// open the thank you modal window instead of the payment one
-					dispatch(openModal({ modalType: 'thankYou' }));
+                if (data.message === successPageTransactionCompleteMessage) {
+                    // open the thank you modal window instead of the payment one
+                    dispatch(openModal({ modalType: 'thankYou' }));
 
-					// refresh funding stats
-					paymentSuccessCb();
-				}
-			} catch (err) {
-				if (err instanceof SyntaxError) {
-					// this just means event.data wasn't a valid json
-				} else {
-					throw err;
-				}
-			}
-		},
-		[dispatch, paymentSuccessCb]
-	);
+                    // refresh funding stats
+                    paymentSuccessCb();
+                }
+            } catch (err) {
+                if (err instanceof SyntaxError) {
+                    // this just means event.data wasn't a valid json
+                } else {
+                    throw err;
+                }
+            }
+        },
+        [dispatch, paymentSuccessCb]
+    );
 
-	useEffect(() => {
-		window.addEventListener('message', handleMessage);
+    useEffect(() => {
+        window.addEventListener('message', handleMessage);
 
-		return () => {
-			window.removeEventListener('message', handleMessage);
-		};
-	}, [handleMessage]);
+        return () => {
+            window.removeEventListener('message', handleMessage);
+        };
+    }, [handleMessage]);
 };
 
 export const useWhoWeAreAnchor = (locationHash, whoWeAreRef) => {
-	useEffect(() => {
-		if (locationHash === '#who-we-are') {
-			// scrollIntoView isn't sufficient since the header hides some of the view
-			const y = whoWeAreRef.current.getBoundingClientRect().top - 100;
-			window.scrollBy({top: y, behavior: 'smooth'});
-		} else {
-			window.scroll({top: 0, behavior: 'smooth'});
-		}
-	}, [locationHash, whoWeAreRef]);
+    useEffect(() => {
+        if (locationHash === '#who-we-are') {
+            // scrollIntoView isn't sufficient since the header hides some of the view
+            const y = whoWeAreRef.current.getBoundingClientRect().top - 100;
+            window.scrollBy({ top: y, behavior: 'smooth' });
+        } else {
+            window.scroll({ top: 0, behavior: 'smooth' });
+        }
+    }, [locationHash, whoWeAreRef]);
 };
 
 export const usePaymentAnchor = (locationHash, paymentRef) => {
-	useEffect(() => {
-		if (locationHash === '#payment') {
-			// scrollIntoView isn't sufficient since the header hides some of the view
-			const y = paymentRef.current.getBoundingClientRect().top - 100;
-			window.scrollBy({top: y, behavior: 'smooth'});
-		} else {
-			window.scroll({top: 0, behavior: 'smooth'});
-		}
-	}, [locationHash, paymentRef]);
+    useEffect(() => {
+        if (locationHash === '#payment') {
+            // scrollIntoView isn't sufficient since the header hides some of the view
+            const y = paymentRef.current.getBoundingClientRect().top - 100;
+            window.scrollBy({ top: y, behavior: 'smooth' });
+        } else {
+            window.scroll({ top: 0, behavior: 'smooth' });
+        }
+    }, [locationHash, paymentRef]);
 };
 
 export const useSectionAnchor = (locationHash, paymentRef, whoWeAreRef) => {
-	useEffect(() => {
-		if (locationHash === '#payment') {
-			// scrollIntoView isn't sufficient since the header hides some of the view
-			const y = paymentRef.current.getBoundingClientRect().top - 100;
-			window.scrollBy({top: y, behavior: 'smooth'});
-		} else if (locationHash === '#who-we-are') {
-			const y = whoWeAreRef.current.getBoundingClientRect().top - 100;
-			window.scrollBy({top: y, behavior: 'smooth'});
-		}
-		else {
-			window.scroll({top: 0, behavior: 'smooth'});
-		}
-	}, [locationHash, paymentRef, whoWeAreRef]);
+    useEffect(() => {
+        if (locationHash === '#payment') {
+            // scrollIntoView isn't sufficient since the header hides some of the view
+            const y = paymentRef.current.getBoundingClientRect().top - 100;
+            window.scrollBy({ top: y, behavior: 'smooth' });
+        } else if (locationHash === '#who-we-are') {
+            const y = whoWeAreRef.current.getBoundingClientRect().top - 100;
+            window.scrollBy({ top: y, behavior: 'smooth' });
+        }
+        else {
+            window.scroll({ top: 0, behavior: 'smooth' });
+        }
+    }, [locationHash, paymentRef, whoWeAreRef]);
 };
