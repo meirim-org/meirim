2869fa465b09837f8e99461ddc6b4683c065c82e
nullhexcode <microooji@icloud.com>
2023-12-26 19:52:21 +0900

    fixed migrations

--- server/migrations/20231105213356_scheduled_tasks.js
+++ server/migrations/20231105213356_scheduled_tasks.js
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
-				return knex.schema.createTable('scheduled_tasks', table => {
-								table.increments('id').primary();
-								table.integer('person_id');
-								table.integer('subscription_id');
-								table.string('date');
-				});
+    return knex.schema.createTable('scheduled_tasks', table => {
+        table.increments('id').primary();
+        table.integer('person_id');
+        table.integer('subscription_id');
+        table.string('date');
+    });
 };
 
 exports.down = function (knex) {
-				return knex.schema.dropTable('scheduled_tasks');
+    return knex.schema.dropTable('scheduled_tasks');
 };
