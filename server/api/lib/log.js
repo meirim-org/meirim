fd032ce138c0db01cdf3269ba22dd07a926d88ee
Yossi Eynav <yossi.eynav@gmail.com>
2024-02-19 13:26:03 +0200

    chore(logs): use util package to send also the splat logs

--- server/api/lib/log.js
+++ server/api/lib/log.js
@@ -1,5 +1,6 @@
 const winston = require('winston');
 const os =  require('os');
+const util = require('util')
 const Config = require('./config');
 
 const apikey = Config.get('coralogix.apikey');
@@ -15,22 +16,32 @@ const logger = winston.createLogger({
 		new winston.transports.Http({
             name: "coralogix",
             level: "info",
-            format: winston.format((info) => ({
-                applicationName: "meirim",
-                subsystemName: serviceName,
-                computerName: os.hostname(),
-                timestamp: Date.now(),
-                severity: {
-                    silly: 1,
-                    debug: 1,
-                    verbose: 2,
-                    info: 3,
-                    warn: 4,
-                    error: 5,
-                    critical: 6
-                }[info.level] || 3,
-                text: info[Symbol.for('message')]
-            }))(),
+            format: winston.format((info) => {
+				const splat = info[Symbol.for('splat')]
+				let text = info[Symbol.for('message')]
+
+				if (splat) {
+					text =  util.format(text, info[Symbol.for('splat')]);
+				}
+				
+				return {
+					applicationName: "meirim",
+					subsystemName: serviceName,
+					computerName: os.hostname(),
+					timestamp: Date.now(),
+					severity: {
+						silly: 1,
+						debug: 1,
+						verbose: 2,
+						info: 3,
+						warn: 4,
+						error: 5,
+						critical: 6
+					}[info.level] || 3,
+					text,
+            }
+			}
+			)(),
             host: host,
             path: "logs/v1/singles",
             headers: {
@@ -45,15 +56,15 @@ const logger = winston.createLogger({
 
 module.exports = {
 	debug: (...args) => {
-		logger.debug(args);
+		logger.debug(...args);
 	},
 	info: (...args) => {
-		logger.info(args);
+		logger.info(...args);
 	},
 	error: (...args) => {
-		logger.error(args);
+		logger.error(...args);
 	},
 	warn: (...args) => {
-		logger.warn(args);
+		logger.warn(...args);
 	}
 };
